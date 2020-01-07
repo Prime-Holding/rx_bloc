2073aa412b48badc98aba295f32733d5bab314c0
Georgi Stanev <georgi.stanev@primeholding.com>
2020-01-07 12:41:20 +0200

    Enhance annotatios

--- lib/annotation/rx_bloc_annotations.dart
+++ lib/annotation/rx_bloc_annotations.dart
@@ -3,13 +3,24 @@ class RxBloc {
 
   final String statesClassName;
 
-  const RxBloc({this.eventsClassName, this.statesClassName});
+  const RxBloc({this.eventsClassName = "Events", this.statesClassName = "States"});
 }
 
-const rxBloc = RxBloc(eventsClassName: null, statesClassName: null);
+const rxBloc = RxBloc();
+
 
 class RxBlocIgnoreState {
   const RxBlocIgnoreState();
 }
 
 const rxBlocIgnoreState = RxBlocIgnoreState();
+
+class RxBlocEvent {
+
+  final RxBlocEventType type;
+  final dynamic seed;
+
+  const RxBlocEvent({this.type = RxBlocEventType.publish, this.seed});
+}
+
+enum RxBlocEventType {behaviour, publish}
