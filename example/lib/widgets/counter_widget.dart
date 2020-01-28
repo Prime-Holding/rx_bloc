91d11f50d1d56dacd97dc7ca291be199fd030560
DimitarKStoyanov <dimitar.stoyanov@luykke-business.ch>
2020-01-28 15:42:42 +0200

    * Add Flutter Driver * Add BDD * Add Page Object and Tests * Add common_functionalities.dart and app_constants.dart

--- example/lib/widgets/counter_widget.dart
+++ example/lib/widgets/counter_widget.dart
@@ -7,57 +7,67 @@ import 'package:flutter_rx_bloc/provider/rx_bloc_listener.dart';
 class CounterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Card(
-        elevation: 2,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-               RxBlocListener<CounterBlocType, String>(
-                 state: (bloc) => bloc.states.infoMessage,
-                 listener: (context, state) =>
-                   Scaffold.of(context).showSnackBar(SnackBar(content: Text(state)))
-               ),
-              Expanded(
-                child: Center(
-                  child: RxBlocBuilder<CounterBlocType, String>(
-                    state: (bloc) => bloc.states.count,
-                    builder: (context, snapshot, bloc) => Text(
-                      snapshot.data ?? '',
-                      style: TextStyle(fontSize: 60),
-                    ),
+    elevation: 2,
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          RxBlocListener<CounterBlocType, String>(
+            state: (bloc) => bloc.states.infoMessage,
+            listener: (context, state) => Scaffold.of(context).showSnackBar(
+              SnackBar(
+                content: Text(state),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: RxBlocBuilder<CounterBlocType, String>(
+                state: (bloc) => bloc.states.count,
+                builder: (context, snapshot, bloc) => Text(
+                  snapshot.data ?? '',
+                  style: TextStyle(fontSize: 60),
+                  key: ValueKey('counter'),
+                ),
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              RxBlocBuilder<CounterBlocType, bool>(
+                state: (bloc) => bloc.states.incrementEnabled,
+                builder: (context, snapshot, bloc) => Expanded(
+                  child: RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    child: Text('Increment'),
+                    onPressed: (snapshot.data ?? false)
+                        ? bloc.events.increment
+                        : null,
+                    key: ValueKey('increment'),
                   ),
                 ),
               ),
-              Row(
-                children: [
-                  RxBlocBuilder<CounterBlocType, bool>(
-                    state: (bloc) => bloc.states.incrementEnabled,
-                    builder: (context, snapshot, bloc) => Expanded(
-                      child: RaisedButton(
-                        child: Text('Increment'),
-                        onPressed: (snapshot.data ?? false)
-                            ? bloc.events.increment
-                            : null,
-                      ),
-                    ),
+              const SizedBox(width: 16),
+              RxBlocBuilder<CounterBlocType, bool>(
+                state: (bloc) => bloc.states.decrementEnabled,
+                builder: (context, snapshot, bloc) => Expanded(
+                  child: RaisedButton(
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    child: Text('Decrement'),
+                    onPressed: (snapshot.data ?? false)
+                        ? bloc.events.decrement
+                        : null,
+                    key: ValueKey('decrement'),
                   ),
-                  const SizedBox(width: 16),
-                  RxBlocBuilder<CounterBlocType, bool>(
-                    state: (bloc) => bloc.states.decrementEnabled,
-                    builder: (context, snapshot, bloc) => Expanded(
-                      child: RaisedButton(
-                        child: Text('Decrement'),
-                        onPressed: (snapshot.data ?? false)
-                            ? bloc.events.decrement
-                            : null,
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ],
           ),
-        ),
-      );
+        ],
+      ),
+    ),
+  );
 }
