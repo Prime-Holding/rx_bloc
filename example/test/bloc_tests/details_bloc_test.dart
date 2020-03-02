4393af859a370794a8fc5b674329defec416c2a8
DDavidPrime <david.djordjevic@primeholding.com>
2020-03-02 14:41:24 +0100

    Add bloc unit tests

---
+++ example/test/bloc_tests/details_bloc_test.dart
@@ -0,0 +1,28 @@
+import 'package:example/bloc/details_bloc.dart';
+import 'package:example/repository/details_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:rx_bloc_test/src/rx_bloc_test.dart';
+
+class MockDetailsRepository extends Mock implements DetailsRepository {}
+
+void main() {
+  group('CounterBloc tests', () {
+    DetailsRepository mockRepo;
+
+    setUp(() {
+      mockRepo = MockDetailsRepository();
+      when(mockRepo.fetch()).thenAnswer((_) => Future.delayed(
+          Duration(milliseconds: 60), () => Future.value('Success')));
+    });
+
+    rxBlocTest<DetailsBloc, String>(
+      'Fetching details',
+      build: () async => DetailsBloc(mockRepo),
+      state: (bloc) => bloc.states.details,
+      act: (bloc) async => bloc.events.fetch(),
+      wait: Duration(milliseconds: 60),
+      expect: ['Success'],
+    );
+  });
+}
