4e86e137588a054f15aaceb5bd5e7343454d4870
Georgi Stanev <georgi.stanev@primeholding.com>
2020-08-19 18:47:00 +0300

    Do not expose LoadingBloc

--- lib/rx_bloc.dart
+++ lib/rx_bloc.dart
@@ -1,7 +1,6 @@
 library rx_bloc;
 
 export 'src/annotation/rx_bloc_annotations.dart';
-export 'src/bloc/loading_bloc.dart';
 export 'src/bloc/rx_bloc_base.dart';
 export 'src/extensions.dart';
 export 'src/model/result.dart';
