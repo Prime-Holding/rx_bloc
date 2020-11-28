0428991fa90d5f727d1fa84d8583dfd37b43ab0b
Georgi Stanev <georgi.stanev@primeholding.com>
2020-11-28 20:32:15 +0200

    Cleaning up

--- packages/rx_bloc/example/main.dart
+++ packages/rx_bloc/example/main.dart
@@ -19,60 +19,36 @@ void main() async {
 
   /// Listen to the loading state
   bloc.states.isLoading.listen((bool isLoading) {
-    print(isLoading ? 'Loading...' : 'Loaded ☑');
+    print(isLoading ? 'Loading...' : 'Loaded ☑ \n');
   });
 
-  await Future.delayed(Duration(milliseconds: 500));
-
   /// Increment once
-  bloc.events.increment();
-
   await Future.delayed(Duration(milliseconds: 500));
+  bloc.events.increment();
 
   /// Increment again.
+  await Future.delayed(Duration(milliseconds: 500));
   bloc.events.increment();
+
+  /// Increment again to cause exception
+  await Future.delayed(Duration(milliseconds: 500));
+  bloc.events.increment();
+
+  /// Decrement
+  await Future.delayed(Duration(milliseconds: 500));
+  bloc.events.decrement();
 }
 
-/// This bloc class usually resides in counter_bloc.dart
-class CounterBloc extends $CounterBloc {
-  CounterBloc(this._server);
-
-  ServerSimulator _server;
-
-  /// Map the count digit to presentable data
-  @override
-  Stream<int> _mapToCountState() =>
-      // we map event to state reactively.
-      _$incrementEvent
-          // Show the initial counter value.
-          .startWith(null)
-          // On increment.
-          .flatMap(
-            (_) => _server
-                .increment()
-                // Converts the Future to a result so we can handle it.
-                .asResultStream(),
-          )
-          // This automatically handles the error and loading state.
-          .setResultStateHandler(this)
-          // Provide success response only.
-          .whereSuccess();
-
-  @override
-  Stream<String> _mapToErrorsState() =>
-      errorState.map((Exception error) => error.toString());
-
-  /// Map event to s
-  @override
-  Stream<bool> _mapToIsLoadingState() => loadingState.skip(3);
-}
-
-/// BLoc class end
+/// This BloC and its event and state contracts usually
+/// resides in counter_bloc.dart
 
 /// A contract class containing all events.
 abstract class CounterBlocEvents {
   /// Increment the count
   void increment();
+
+  /// Decrement the count
+  void decrement();
 }
 
 /// A contract class containing all states for our multi state BloC.
@@ -91,12 +67,72 @@ abstract class CounterBlocStates {
   Stream<String> get errors;
 }
 
-/// ********************GENERATED CODE**************************************
-/// NO NEED TO ANALYZE IT
-/// This is a code that will be automatically generated for you by the rx_bloc_generator
+class CounterBloc extends $CounterBloc {
+  CounterBloc(this._server);
+
+  ServerSimulator _server;
+
+  /// Map increment and decrement events to `count` state
+  @override
+  Stream<int> _mapToCountState() => Rx.merge<Result<int>>([
+        // On increment.
+        _$incrementEvent.flatMap((_) => _server.increment().asResultStream()),
+        // On decrement.
+        _$decrementEvent.flatMap((_) => _server.decrement().asResultStream()),
+      ])
+          // This automatically handles the error and loading state.
+          .setResultStateHandler(this)
+          // Provide success response only.
+          .whereSuccess()
+          //emit 0 as initial value
+          .startWith(0);
+
+  @override
+  Stream<String> _mapToErrorsState() =>
+      errorState.map((Exception error) => error.toString());
+
+  @override
+  Stream<bool> _mapToIsLoadingState() => loadingState;
+}
+
+/// BLoc class end
+
+/// This will simulate a server with 100 milliseconds response time
+class ServerSimulator {
+  int _counter = 0;
+
+  Future<int> increment() async {
+    // Server response time.
+    await Future.delayed(Duration(milliseconds: 100));
+    // Simulate an error from the server when the counter reached 2.
+    if (_counter == 2) {
+      throw Exception('Maximum number is reached!');
+    }
+
+    return ++_counter;
+  }
+
+  Future<int> decrement() async {
+    // Server response time.
+    await Future.delayed(Duration(milliseconds: 100));
+    // Simulate an error from the server when the counter reached 2.
+    if (_counter <= 0) {
+      throw Exception('Minimum number is reached!');
+    }
+
+    return --_counter;
+  }
+}
+
+/// NO NEED TO ANALYZE IT!
+///
+/// The code below will be automatically generated
+/// for you by `rx_bloc_generator`.
+///
 /// This generated class usually resides in [file-name].rxb.g.dart.
 /// Find more info at https://pub.dev/packages/rx_bloc_generator.
 
+/// ********************GENERATED CODE**************************************
 /// CounterBlocType class used for bloc event and state access from widgets
 abstract class CounterBlocType extends RxBlocTypeBase {
   CounterBlocEvents get events;
@@ -107,6 +143,11 @@ abstract class CounterBlocType extends RxBlocTypeBase {
 /// $CounterBloc class - extended by the CounterBloc bloc
 abstract class $CounterBloc extends RxBlocBase
     implements CounterBlocEvents, CounterBlocStates, CounterBlocType {
+  final _$decrementEvent = PublishSubject<void>();
+
+  @override
+  void decrement() => _$decrementEvent.add(null);
+
   final _$incrementEvent = PublishSubject<void>();
 
   @override
@@ -147,19 +188,3 @@ abstract class $CounterBloc extends RxBlocBase
 }
 
 /// ********************GENERATED CODE END**************************************
-
-/// This will simulate a server with 100 milliseconds response time
-class ServerSimulator {
-  int _counter = 0;
-
-  Future<int> increment() async {
-    // Server response time.
-    await Future.delayed(Duration(milliseconds: 100));
-    // Simulate an error from the server when the counter reached 2.
-    if (_counter == 2) {
-      throw Exception('Maximum number is reached!');
-    }
-
-    return _counter++;
-  }
-}
