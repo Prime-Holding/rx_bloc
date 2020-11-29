b6b97dc0e0f30db05152a59d7d54e271138eaef9
Yanko Diev <62933904+YankoDiev-PrimeHolding@users.noreply.github.com>
2020-11-29 10:44:02 +0200

    New rx_bloc simple example (#6)
    * New rx_bloc simple example
    
    * Cleaning up
    
    * Implement counter sample in flutter_rx_bloc
    
    * Comments corrections
    
    Co-authored-by: Georgi Stanev <georgi.stanev@primeholding.com>

---
+++ packages/flutter_rx_bloc/example/counter/lib/counter_bloc.rxb.g.dart
@@ -0,0 +1,64 @@
+part of 'main.dart';
+
+/// The code below will be automatically generated
+/// for you by `rx_bloc_generator`.
+///
+/// This generated class usually resides in [file-name].rxb.g.dart.
+/// Find more info at https://pub.dev/packages/rx_bloc_generator.
+
+/// ********************GENERATED CODE**************************************
+/// CounterBlocType class used for bloc event and state access from widgets
+abstract class CounterBlocType extends RxBlocTypeBase {
+  CounterBlocEvents get events;
+
+  CounterBlocStates get states;
+}
+
+/// $CounterBloc class - extended by the CounterBloc bloc
+abstract class $CounterBloc extends RxBlocBase
+    implements CounterBlocEvents, CounterBlocStates, CounterBlocType {
+  final _$decrementEvent = PublishSubject<void>();
+
+  @override
+  void decrement() => _$decrementEvent.add(null);
+
+  final _$incrementEvent = PublishSubject<void>();
+
+  @override
+  void increment() => _$incrementEvent.add(null);
+
+  Stream<int> _countState;
+
+  @override
+  Stream<int> get count => _countState ??= _mapToCountState();
+
+  Stream<int> _mapToCountState();
+
+  Stream<bool> _isLoadingState;
+
+  @override
+  Stream<bool> get isLoading => _isLoadingState ??= _mapToIsLoadingState();
+
+  Stream<bool> _mapToIsLoadingState();
+
+  Stream<String> _errorsState;
+
+  @override
+  Stream<String> get errors => _errorsState ??= _mapToErrorsState();
+
+  Stream<String> _mapToErrorsState();
+
+  @override
+  CounterBlocEvents get events => this;
+
+  @override
+  CounterBlocStates get states => this;
+
+  /// Dispose of all the opened streams when the bloc is closed.
+  void dispose() {
+    _$incrementEvent.close();
+    super.dispose();
+  }
+}
+
+/// ********************GENERATED CODE END**************************************
