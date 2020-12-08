a8870c60b900933010ba9b7c32f9d5b39a510bb7
Georgi Stanev <georgi.stanev@primeholding.com>
2020-12-08 10:31:02 +0200

    Fix unit tests

--- examples/division/test/division_bloc_test.dart
+++ examples/division/test/division_bloc_test.dart
@@ -1,22 +1,22 @@
+import 'package:example/bloc/division_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rx_bloc_test/rx_bloc_test.dart';
-import 'package:example/bloc/division_bloc.dart';
 
 void main() {
   group('DivisionBloc tests', () {
     final String num1 = '10';
     final String num2 = '5';
-    final String expectedSolution = '2';
+    final String expectedSolution = '10.0 / 5.0 = 2.0';
 
-    /*
     rxBlocTest<DivisionBloc, String>(
       'DivisionBloc divides successfully two numbers',
       build: () async => DivisionBloc(),
       state: (bloc) => bloc.states.divisionResult,
-      act: (bloc) async => bloc.events.divideNumbers(num1, num2),
-      skip: 0,
+      act: (bloc) async {
+        bloc.events.divideNumbers(num1, num2);
+        await Future.delayed(Duration(milliseconds: 1002));
+      },
       expect: [expectedSolution],
     );
-    */
   });
 }
