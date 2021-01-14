1e034d4ee81838cdad2c32640a841bc2eb7617a7
Georgi Stanev <georgi.stanev@primeholding.com>
2021-01-14 09:51:57 +0200

    Update the counter sample

--- examples/counter/lib/bloc/counter_bloc.dart
+++ examples/counter/lib/bloc/counter_bloc.dart
@@ -1,9 +1,10 @@
+import 'package:example/repository/counter_repository.dart';
 import 'package:rx_bloc/rx_bloc.dart';
 import 'package:rxdart/rxdart.dart';
 
 part 'counter_bloc.rxb.g.dart';
 
-/// A class containing all incoming events to the BloC
+/// A contract class containing all events.
 abstract class CounterBlocEvents {
   /// Increment the count
   void increment();
@@ -12,74 +13,51 @@ abstract class CounterBlocEvents {
   void decrement();
 }
 
-/// A class containing all states (outputs) of the bloc.
+/// A contract class containing all states for our multi state BloC.
 abstract class CounterBlocStates {
   /// The count of the Counter
   ///
   /// It can be controlled by executing [CounterBlocEvents.increment] and
   /// [CounterBlocEvents.decrement]
   ///
-  Stream<String> get count;
+  Stream<int> get count;
 
-  /// The state of the increment action control
-  Stream<bool> get incrementEnabled;
+  /// Loading state
+  Stream<bool> get isLoading;
 
-  /// The state of the decrement action control
-  Stream<bool> get decrementEnabled;
-
-  /// The info message caused by changing action controls' state
-  Stream<String> get infoMessage;
+  /// Error messages
+  Stream<String> get errors;
 }
 
+/// A BloC responsible for count calculations
 @RxBloc()
 class CounterBloc extends $CounterBloc {
-  /// The internal storage of the count
-  final _count = BehaviorSubject.seeded(0);
+  /// Default constructor
+  CounterBloc(this._repository);
 
-  /// Acts as a container for multiple subscriptions that can be canceled at once
-  final _compositeSubscription = CompositeSubscription();
+  final CounterRepository _repository;
 
-  CounterBloc() {
-    MergeStream([
-      _$incrementEvent.map((_) => _count.value + 1),
-      _$decrementEvent.map((_) => _count.value - 1)
-    ]).bind(_count).disposedBy(_compositeSubscription);
-  }
-
-  /// Map the count digit to presentable data
+  /// Map increment and decrement events to `count` state
   @override
-  Stream<String> _mapToCountState() => _count.map((count) => count.toString());
-
-  /// Map the count digit to a decrement enabled state.
-  @override
-  Stream<bool> _mapToDecrementEnabledState() =>
-      _count.map((count) => count > 0);
-
-  /// Map the count digit to a increment enabled state.
-  @override
-  Stream<bool> _mapToIncrementEnabledState() =>
-      _count.map((count) => count < 5);
-
-  /// Map the increment and decrement enabled state to a informational message.
-  @override
-  Stream<String> _mapToInfoMessageState() => MergeStream([
-        incrementEnabled.mapToMaximumMessage(),
-        decrementEnabled.mapToMinimumMessage(),
-      ]).skip(1).throttleTime(Duration(seconds: 1));
+  Stream<int> _mapToCountState() => Rx.merge<Result<int>>([
+        // On increment.
+        _$incrementEvent
+            .flatMap((_) => _repository.increment().asResultStream()),
+        // On decrement.
+        _$decrementEvent
+            .flatMap((_) => _repository.decrement().asResultStream()),
+      ])
+          // This automatically handles the error and loading state.
+          .setResultStateHandler(this)
+          // Provide success response only.
+          .whereSuccess()
+          //emit 0 as initial value
+          .startWith(0);
 
   @override
-  void dispose() {
-    _compositeSubscription.dispose();
-    super.dispose();
-  }
-}
+  Stream<String> _mapToErrorsState() =>
+      errorState.map((Exception error) => error.toString());
 
-extension _InfoMessage on Stream<bool> {
-  /// Map disabled state to a informational message
-  Stream<String> mapToMaximumMessage() => where((enabled) => !enabled)
-      .map((_) => "You have reached the maximum increment count");
-
-  /// Map disabled state to a informational message
-  Stream<String> mapToMinimumMessage() => where((enabled) => !enabled)
-      .map((_) => "You have reached the minimum decrement count");
+  @override
+  Stream<bool> _mapToIsLoadingState() => loadingState;
 }
