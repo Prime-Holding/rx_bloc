b2036257e177e00043bce05f03e77f5c58f61b9b
Stanislav Petkov <stanislav.petkov@primeholding.com>
2021-03-11 11:06:17 +0200

    add HomePage with an empty PuppiesAppBar

---
+++ examples/favorites_advanced/bloc_favorites_advanced/lib/base/routers/router.dart
@@ -0,0 +1,25 @@
+import 'package:auto_route/auto_route_annotations.dart';
+import 'package:bloc_sample/feature_home/views/home_page.dart';
+
+@MaterialAutoRouter(
+  routes: <AutoRoute>[
+    MaterialRoute(page: HomePage, initial: true),
+  ],
+)
+class $MyRouter {}
+
+
+// import 'package:auto_route/auto_route_annotations.dart';
+// // import 'package:rx_bloc_favorites_advanced/feature_puppy/details/views/puppy_details_page.dart';
+// // import 'package:rx_bloc_favorites_advanced/feature_puppy/edit/views/puppy_edit_page.dart';
+//
+// import '../../feature_home/views/home_page.dart';
+//
+// @MaterialAutoRouter(
+//   routes: <AutoRoute>[
+//     MaterialRoute(page: HomePage, initial: true),
+//     // MaterialRoute(page: PuppyDetailsPage),
+//     // MaterialRoute(page: PuppyEditPage),
+//   ],
+// )
+// class $MyRouter {}
---
+++ examples/favorites_advanced/bloc_favorites_advanced/lib/feature_home/views/home_page.dart
@@ -0,0 +1,132 @@
+
+import 'package:bloc_sample/base/ui_components/puppies_app_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar:  PuppiesAppBar(),
+  );
+}
+
+// import 'package:auto_route/auto_route.dart';
+// import 'package:badges/badges.dart';
+// import 'package:bloc_sample/base/resources/keys.dart';
+// import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+// import 'package:favorites_advanced_base/extensions.dart';
+// import 'package:favorites_advanced_base/models.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_rx_bloc/flutter_rx_bloc.dart';
+// import 'package:provider/provider.dart';
+// import 'package:rx_bloc_favorites_advanced/base/resources/keys.dart';
+//
+// import '../../base/extensions/async_snapshot.dart';
+// import '../../base/ui_components/puppies_app_bar.dart';
+// import '../../feature_puppy/blocs/puppies_extra_details_bloc.dart';
+// import '../../feature_puppy/blocs/puppy_manage_bloc.dart';
+// import '../../feature_puppy/favorites/blocs/favorite_puppies_bloc.dart';
+// import '../../feature_puppy/favorites/views/favorites_page.dart';
+// import '../../feature_puppy/search/views/search_page.dart';
+// import '../blocs/navigation_bar_bloc.dart';
+//
+// part 'home_providers.dart';
+//
+// class HomePage extends StatelessWidget with AutoRouteWrapper {
+//   HomePage({Key key}) : super(key: key);
+//
+//   @override
+//   Widget wrappedRoute(BuildContext context) => RxMultiBlocProvider(
+//         providers: _getProviders(),
+//         child: this,
+//       );
+//
+//   @override
+//   Widget build(BuildContext context) => Scaffold(
+//         resizeToAvoidBottomInset: false,
+//         // resizeToAvoidBottomInset: false,
+//         appBar: PuppiesAppBar(),
+//         body: RxBlocListener<PuppyManageBlocType, String>(
+//           state: (bloc) => bloc.states.error,
+//           listener: (ctx, state) =>
+//               Scaffold.of(ctx).showSnackBar(SnackBar(content: Text(state))),
+//           child: Stack(
+//             alignment: Alignment.bottomCenter,
+//             children: [
+//               // _buildBody(),
+//               _buildNavBar(),
+//             ],
+//           ),
+//         ),
+//       );
+//
+//   // RxBlocBuilder<NavigationBarBlocType, NavigationItem> _buildBody() =>
+//   //     RxBlocBuilder<NavigationBarBlocType, NavigationItem>(
+//   //       key: const ValueKey(Keys.puppyHomePage),
+//   //       state: (bloc) => bloc.states.selectedItem,
+//   //       builder: (ctx, snapshot, bloc) => AnimatedSwitcher(
+//   //         duration: const Duration(milliseconds: 200),
+//   //         child: asPage(snapshot),
+//   //       ),
+//   //     );
+//
+//   RxBlocBuilder<NavigationBarBlocType, List<NavigationItem>> _buildNavBar() =>
+//       RxBlocBuilder<NavigationBarBlocType, List<NavigationItem>>(
+//           state: (bloc) => bloc.states.items,
+//           builder: (context, snapshot, bloc) =>
+//               snapshot.build((navItems) => CurvedNavigationBar(
+//                     index: navItems.toCurrentIndex(),
+//                     color: Colors.blueAccent,
+//                     backgroundColor: Colors.transparent,
+//                     items: navItems
+//                         .map((item) => Padding(
+//                               child: item.asWidget(),
+//                               padding: const EdgeInsets.all(8),
+//                             ))
+//                         .toList(),
+//                     onTap: (index) => bloc.events.selectPage(
+//                       index == 0
+//                           ? NavigationItemType.search
+//                           : NavigationItemType.favorites,
+//                     ),
+//                   )));
+//
+//   // Widget asPage(AsyncSnapshot<NavigationItem> type) {
+//   //   if (!type.hasData) {
+//   //     return Container();
+//   //   }
+//   //
+//   //   switch (type.data.type) {
+//   //     case NavigationItemType.search:
+//   //       return SearchPage();
+//   //     case NavigationItemType.favorites:
+//   //       return FavoritesPage();
+//   //   }
+//   //
+//   //   return Container();
+//   // }
+// }
+//
+// extension NavigationItemToWitget on NavigationItem {
+//   Widget asWidget() => type == NavigationItemType.favorites
+//       ? RxBlocBuilder<FavoritePuppiesBlocType, int>(
+//           state: (bloc) => bloc.states.count,
+//           builder: (ctx, snapshot, bloc) =>
+//               snapshot.hasData && snapshot.data <= 0
+//                   ? type.asIcon()
+//                   : Badge(
+//                       padding: const EdgeInsets.all(3),
+//                       badgeContent: snapshot.build((count) => Text(
+//                             count.toString(),
+//                             style: const TextStyle(
+//                               color: Colors.white,
+//                               fontSize: 12,
+//                             ),
+//                           )),
+//                       badgeColor: Colors.transparent,
+//                       elevation: 0,
+//                       child: type.asIcon(),
+//                     ),
+//         )
+//       : type.asIcon();
+// }
