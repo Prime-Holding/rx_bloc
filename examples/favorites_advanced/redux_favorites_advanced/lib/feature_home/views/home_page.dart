2a3760faf292e46bc2a69be1e794b25f1dc67d30
Goran Đukić <goran.djukic@primeholding.com>
2021-03-16 16:28:25 +0100

    Create store in main() instead of the build function

--- examples/favorites_advanced/redux_favorites_advanced/lib/feature_home/views/home_page.dart
+++ examples/favorites_advanced/redux_favorites_advanced/lib/feature_home/views/home_page.dart
@@ -19,23 +19,20 @@ class HomePage extends StatelessWidget {
   final Store<NavigationState> store;
 
   @override
-  Widget build(BuildContext context) => StoreProvider<NavigationState>(
-        store: store,
-        child: Scaffold(
-          appBar: PuppiesAppBar(),
-          bottomNavigationBar: CurvedNavigationBar(
-            color: Colors.blueAccent,
-            backgroundColor: Colors.white,
-            items: store.state.items.map((item) => item.type.asIcon()).toList(),
-            onTap: (index) => store.dispatch(
-              index == 0 ? SearchViewAction() : FavoritesViewAction(),
-            ),
+  Widget build(BuildContext context) => Scaffold(
+        appBar: PuppiesAppBar(),
+        bottomNavigationBar: CurvedNavigationBar(
+          color: Colors.blueAccent,
+          backgroundColor: Colors.white,
+          items: store.state.items.map((item) => item.type.asIcon()).toList(),
+          onTap: (index) => store.dispatch(
+            index == 0 ? SearchViewAction() : FavoritesViewAction(),
           ),
-          body: StoreBuilder<NavigationState>(
-            builder: (_, store) => store.state.items.toCurrentIndex() == 0
-                ? SearchView()
-                : FavoritesView(),
-          ),
+        ),
+        body: StoreBuilder<NavigationState>(
+          builder: (_, store) => store.state.items.toCurrentIndex() == 0
+              ? SearchView()
+              : FavoritesView(),
         ),
       );
 }
--- examples/favorites_advanced/redux_favorites_advanced/lib/main.dart
+++ examples/favorites_advanced/redux_favorites_advanced/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
 
 import 'feature_home/models/navigation_state.dart';
@@ -7,22 +8,28 @@ import 'feature_home/redux/reducers.dart';
 import 'feature_home/views/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  final store = Store<NavigationState>(
+    navStateReducer,
+    initialState: NavigationState.initialState(),
+  );
+
+  runApp(MyApp(store));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp(this.store);
+
+  final Store<NavigationState> store;
+
   @override
-  Widget build(BuildContext context) {
-    final store = Store<NavigationState>(
-      navStateReducer,
-      initialState: NavigationState.initialState(),
-    );
-    return MaterialApp(
-      title: 'Puppy Redux',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(store),
-    );
-  }
+  Widget build(BuildContext context) => StoreProvider<NavigationState>(
+        store: store,
+        child: MaterialApp(
+          title: 'Puppy Redux',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: HomePage(store),
+        ),
+      );
 }
