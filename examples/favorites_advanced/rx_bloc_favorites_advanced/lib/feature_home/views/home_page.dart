26fe884df0a2ee78bb8812e2c61932d0c81b9eaf
Snezhana Romanova <snezhana.romanova@primeholding.com>
2021-03-16 10:41:42 +0200

    Replays resizeToAvoidBottomPadding with resizeToAvoidBottomInset, in HomePage, Scaffold

--- examples/favorites_advanced/rx_bloc_favorites_advanced/lib/feature_home/views/home_page.dart
+++ examples/favorites_advanced/rx_bloc_favorites_advanced/lib/feature_home/views/home_page.dart
@@ -30,8 +30,7 @@ class HomePage extends StatelessWidget with AutoRouteWrapper {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        // resizeToAvoidBottomPadding: false,
-        // resizeToAvoidBottomInset: false,
+        resizeToAvoidBottomInset: false,
         appBar: PuppiesAppBar(),
         body: RxBlocListener<PuppyManageBlocType, String>(
           state: (bloc) => bloc.states.error,
