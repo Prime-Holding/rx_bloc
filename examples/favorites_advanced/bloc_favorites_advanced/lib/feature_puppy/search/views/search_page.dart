3c494631ccebc8b18c0cdc3804a189c8a5f82f55
Stanislav Petkov <stanislav.petkov@primeholding.com>
2021-03-19 16:25:03 +0200

    Add Circular progress indicator when loading puppies

--- examples/favorites_advanced/bloc_favorites_advanced/lib/feature_puppy/search/views/search_page.dart
+++ examples/favorites_advanced/bloc_favorites_advanced/lib/feature_puppy/search/views/search_page.dart
@@ -11,31 +11,48 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) =>
       BlocBuilder<PuppyListBloc, PuppyListState>(
-        key: const Key(Keys.puppySearchPage),
-        builder: (context, state) => RefreshIndicator(
-          onRefresh: () {
-            context
-                .read<PuppyListBloc>()
-                .add(ReloadFavoritePuppies(silently: true));
-            return Future.delayed(const Duration(seconds: 1));
-          },
-          child: SafeArea(
-            child: ListView.builder(
-              padding: const EdgeInsets.only(bottom: 67),
-              itemCount: state.searchedPuppies.length,
-              itemBuilder: (context, index) {
-                final item = state.searchedPuppies[index];
-                // context.read<PuppyListBloc>().searchedPuppies[index];
-                return PuppyCard(
-                  key: Key('${Keys.puppyCardNamePrefix}${item.id}'),
-                  // onVisible: (puppy) => context
-                  //       .read<PuppiesExtraDetailsBloc>()
-                  //       .add(FetchPuppiesExtraDetailsEvent(puppy)),
-                  puppy: item,
+          key: const Key(Keys.puppySearchPage),
+          builder: (context, state) {
+            switch (state.status) {
+              case PuppyListStatus.failure:
+                return const Center(child: Text('failed to fetch puppies'));
+              case PuppyListStatus.success:
+                return RefreshIndicator(
+                  onRefresh: () {
+                    context
+                        .read<PuppyListBloc>()
+                        .add(ReloadFavoritePuppies(silently: true));
+                    return Future.delayed(const Duration(seconds: 1));
+                  },
+                  child: SafeArea(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.only(bottom: 67),
+                      itemCount: state.searchedPuppies.length,
+                      itemBuilder: (context, index) {
+                        final item = state.searchedPuppies[index];
+                        // context.read<PuppyListBloc>().add(LoadPuppyListEvent());
+
+                        return PuppyCard(
+                          key: Key('${Keys.puppyCardNamePrefix}${item.id}'),
+                          // onVisible: (puppy) => context
+                          //       .read<PuppiesExtraDetailsBloc>()
+                          //       .add(FetchPuppiesExtraDetailsEvent(puppy)),
+                          puppy: item,
+                        );
+                      },
+                    ),
+                  ),
                 );
-              },
-            ),
-          ),
-        ),
-      );
+              // default:
+              case PuppyListStatus.loading:
+                context.read<PuppyListBloc>().add(LoadPuppyListEvent());
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              default:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+            }
+          });
 }
