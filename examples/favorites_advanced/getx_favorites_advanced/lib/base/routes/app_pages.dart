bd28954738b0957bdb217dbeb4239695b6cc73b4
RomanovaPrime <snezhana.romanova@primeholding.com>
2021-04-01 01:50:19 +0300

    Solve analyzer suggestion

--- examples/favorites_advanced/getx_favorites_advanced/lib/base/routes/app_pages.dart
+++ examples/favorites_advanced/getx_favorites_advanced/lib/base/routes/app_pages.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 
-import 'file:///C:/Users/snezh/AndroidStudioProjects/rx_bloc/examples/favorites_advanced/getx_favorites_advanced/lib/feature_home/bindings/home_binding.dart';
+import 'package:getx_favorites_advanced/feature_home/bindings/home_binding.dart';
 import 'package:getx_favorites_advanced/feature_home/views/home_page.dart';
 
 part 'app_routes.dart';
