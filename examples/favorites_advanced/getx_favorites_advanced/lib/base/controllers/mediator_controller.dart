ec082cbcf94423f77a943f9057da6a99ff6e3dd6
RomanovaPrime <snezhana.romanova@primeholding.com>
2021-04-05 11:48:25 +0300

    Change name base_controller to mediator_controller

---
+++ examples/favorites_advanced/getx_favorites_advanced/lib/base/controllers/mediator_controller.dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+
+import 'package:favorites_advanced_base/core.dart';
+
+class MediatorController extends GetxController {
+  MediatorController();
+
+  final lastFetchedPuppiesLocal = <Puppy>[].obs;
+  final puppiesToChangeFavoriteStatus = <Puppy>[].obs;
+  final toClearFetchedExtraDetails = false.obs;
+
+  void updatePuppiesWithExtraDetails(RxList<Puppy> lastFetchedPuppies) =>
+      lastFetchedPuppiesLocal.assignAll(lastFetchedPuppies);
+
+  void puppiesUpdated(List<Puppy> puppiesToUpdate) =>
+      puppiesToChangeFavoriteStatus.assignAll(puppiesToUpdate);
+
+  void puppyUpdated(Puppy puppy) => puppiesUpdated([puppy]);
+
+  void clearFetchedExtraDetails() => toClearFetchedExtraDetails(true);
+}
