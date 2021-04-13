9b90668d182a05bc5bfe738dc617482c5683c9d5
David Djordjevic <david.djordjevic@primeholding.com>
2021-04-13 14:01:18 +0200

    Add initial rx_bloc_cli project

---
+++ packages/rx_bloc_cli/lib/rx_bloc_cli.dart
@@ -0,0 +1,20 @@
+/// Rx Bloc Command Line Interface
+
+///
+/// ```sh
+/// # activate rx_bloc_cli
+/// pub global activate rx_bloc_cli
+///
+/// # see usage
+/// rx_bloc_cli --help
+/// ```
+///
+
+library rx_bloc_cli;
+
+/*
+Useful testing commands:
+pub global activate -s path ~/repos/rx_bloc/packages/rx_bloc_cli --overwrite
+pub global deactivate rx_bloc_cli
+pub global list
+*/
---
+++ packages/rx_bloc_cli/lib/src/command_runner.dart
@@ -0,0 +1,54 @@
+import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
+import 'package:io/io.dart';
+import 'package:mason/mason.dart';
+
+import 'rx_bloc_command_exception.dart';
+
+class RxBlocCommandRunner extends CommandRunner<int> {
+  RxBlocCommandRunner({Logger logger})
+      : _logger = logger ?? Logger(),
+        super('rx_bloc_cli', 'Rx Bloc Command Line Interface') {
+    argParser.addFlag(
+      'version',
+      abbr: 'v',
+      negatable: false,
+      help: 'Print the current version.',
+    );
+  }
+
+  final Logger _logger;
+  ArgResults _argResults;
+
+  @override
+  Future<int> run(Iterable<String> args) async {
+    try {
+      _argResults = parse(args);
+      return await runCommand(_argResults) ?? ExitCode.success.code;
+    } on FormatException catch (e) {
+      _logger
+        ..err(e.message)
+        ..info('')
+        ..info(usage);
+      return ExitCode.usage.code;
+    } on UsageException catch (e) {
+      _logger
+        ..err(e.message)
+        ..info('')
+        ..info(usage);
+      return ExitCode.usage.code;
+    } on RxBlocCommandException catch (e) {
+      _logger.err(e.message);
+      return ExitCode.usage.code;
+    }
+  }
+
+  @override
+  Future<int> runCommand(ArgResults topLevelResults) async {
+    if (topLevelResults['version'] == true) {
+      _logger.info('package version: 0.0.1');
+      return ExitCode.success.code;
+    }
+    return super.runCommand(topLevelResults);
+  }
+}
