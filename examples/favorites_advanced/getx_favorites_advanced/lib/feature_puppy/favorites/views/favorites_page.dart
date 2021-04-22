e0424485bc7a61513fab1bb100dae71f212439c1
RomanovaPrime <snezhana.romanova@primeholding.com>
2021-04-22 15:20:45 +0300

    Use custom AnimatedList from Stanislav Petkov

--- examples/favorites_advanced/getx_favorites_advanced/lib/feature_puppy/favorites/views/favorites_page.dart
+++ examples/favorites_advanced/getx_favorites_advanced/lib/feature_puppy/favorites/views/favorites_page.dart
@@ -3,25 +3,32 @@ import 'package:get/get.dart';
 
 import 'package:favorites_advanced_base/core.dart';
 import 'package:favorites_advanced_base/resources.dart';
+import 'package:getx_favorites_advanced/base/ui_components/puppy_animated_list_view.dart';
 
-import 'package:getx_favorites_advanced/base/ui_components/puppy_automatic_animated_list.dart';
+import 'package:getx_favorites_advanced/feature_puppy/controllers/puppy_manage_controller.dart';
 import 'package:getx_favorites_advanced/feature_puppy/favorites/controllers/favorite_puppies_controller.dart';
 
 class FavoritesPage extends GetView<FavoritePuppiesController> {
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: controller.obx(
-          (_) => Container(
-            key: const ValueKey(Keys.puppyFavoritesPage),
-            child: Obx(
-              () => PuppyAutomaticAnimatedListView(
+        body: Column(
+          key: const ValueKey(Keys.puppyFavoritesPage),
+          children: [
+            Expanded(
+              child: PuppyAnimatedListView(
                 puppyList: controller.favoritePuppiesList,
+                onFavorite: (puppy, isFavotire) =>
+                    Get.find<PuppyManageController>()
+                        .markAsFavorite(puppy: puppy, isFavorite: isFavotire),
               ),
             ),
-          ),
-          onError: (error) => ErrorRetryWidget(
-            onReloadTap: () => controller.onReload(),
-          ),
+            controller.obx(
+              (state) => Container(),
+              onError: (_) => ErrorRetryWidget(
+                onReloadTap: () => controller.onReload(),
+              ),
+            )
+          ],
         ),
       );
 }
