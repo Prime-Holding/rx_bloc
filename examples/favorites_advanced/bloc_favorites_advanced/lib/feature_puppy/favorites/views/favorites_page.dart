a9ff7b74916432948d217d8002c9fa25285e31f6
Stanislav Petkov <stanislav.petkov@primeholding.com>
2021-04-23 09:49:40 +0300

    Change to PuppyAnimatedListView from favorites_advanced_base

--- examples/favorites_advanced/bloc_favorites_advanced/lib/feature_puppy/favorites/views/favorites_page.dart
+++ examples/favorites_advanced/bloc_favorites_advanced/lib/feature_puppy/favorites/views/favorites_page.dart
@@ -1,7 +1,4 @@
 import 'package:bloc_sample/feature_puppy/favorites/blocs/favorite_puppies_bloc.dart';
-import 'package:bloc_sample/feature_puppy/list/ui_component/puppy_animated_list_view.dart';
-import 'package:declarative_animated_list/declarative_animated_list.dart';
-import 'package:favorites_advanced_base/models.dart';
 import 'package:favorites_advanced_base/ui_components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,61 +13,15 @@ class FavoritesPage extends StatelessWidget {
         children: [
           Expanded(
             child: BlocBuilder<FavoritePuppiesBloc, FavoritePuppiesState>(
-              builder: (context, state) => DeclarativeList<Puppy>(
-                items: state.favoritePuppies,
-                itemBuilder: (ctx, puppy, index, animation) =>
-                    _puppyTile(puppy, animation, ctx),
-                removeBuilder: (ctx, puppy, index, animation) =>
-                    _puppyTile(puppy, animation, ctx),
+              builder: (context, state) => PuppyAnimatedListView(
+                puppyList: state.favoritePuppies,
+                onFavorite: (puppy, isFavorite) =>
+                    BlocProvider.of<FavoritePuppiesBloc>(context).add(
+                        FavoritePuppiesMarkAsFavoriteEvent(
+                            puppy: puppy, isFavorite: isFavorite)),
               ),
             ),
           ),
         ],
       );
-
-  Widget _puppyTile(
-          Puppy puppy, Animation<double> animation, BuildContext context) =>
-      SizeTransition(
-        sizeFactor: animation,
-        child: PuppyCard(
-          key: Key('${Keys.puppyCardNamePrefix}${puppy.id}'),
-          puppy: puppy,
-          onCardPressed: (puppy) {},
-          onFavorite: (puppy, isFavorite) =>
-              BlocProvider.of<FavoritePuppiesBloc>(context).add(
-                  FavoritePuppiesMarkAsFavoriteEvent(
-                      puppy: puppy, isFavorite: isFavorite)),
-        ),
-      );
 }
-
-/*
-import 'package:bloc_sample/feature_puppy/favorites/blocs/favorite_puppies_bloc.dart';
-import 'package:bloc_sample/feature_puppy/list/ui_component/puppy_animated_list_view.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:favorites_advanced_base/resources.dart';
-
-class FavoritesPage extends StatelessWidget {
-  static const favoritesPage = 'Favorites page';
-
-  @override
-  Widget build(BuildContext context) => Column(
-        key: const ValueKey(Keys.puppyFavoritesPage),
-        children: [
-          Expanded(
-            child: PuppyAnimatedListView(
-              puppyList: BlocProvider.of<FavoritePuppiesBloc>(context)
-                  .state
-                  .favoritePuppies,
-              onFavorite: (puppy, isFavorite) =>
-                  context.read<FavoritePuppiesBloc>().add(
-                        FavoritePuppiesMarkAsFavoriteEvent(
-                            puppy: puppy, isFavorite: isFavorite),
-                      ),
-            ),
-          ),
-        ],
-      );
-}
- */
