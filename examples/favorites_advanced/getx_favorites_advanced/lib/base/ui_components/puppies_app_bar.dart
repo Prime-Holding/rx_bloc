e328d580bfc289579f490e70786e31ed06ec8474
RomanovaPrime <snezhana.romanova@primeholding.com>
2021-04-29 17:34:35 +0300

    Fill searching string in title if puppies are filtered

--- examples/favorites_advanced/getx_favorites_advanced/lib/base/ui_components/puppies_app_bar.dart
+++ examples/favorites_advanced/getx_favorites_advanced/lib/base/ui_components/puppies_app_bar.dart
@@ -11,7 +11,7 @@ class PuppiesAppBar extends StatelessWidget implements PreferredSizeWidget {
   final controller = Get.find<PuppyListController>();
 
   AppBarTextField _searchAppBar() => AppBarTextField(
-        title: const Text('Search for Puppies'),
+        title: _createTitle(),
         style: const TextStyle(color: Colors.white),
         autofocus: false,
         decoration: const InputDecoration(
@@ -40,6 +40,16 @@ class PuppiesAppBar extends StatelessWidget implements PreferredSizeWidget {
       Get.focusScope?.unfocus();
     }
   }
+
+  Widget _createTitle() =>
+      controller.filteredBy.value != ''
+        ? Row(
+          children: [
+            const SizedBox(width: 30,),
+            Text('${controller.filteredBy}...'),
+          ],
+        )
+        :const Text('Search for Puppies');
 
   @override
   Widget build(BuildContext context) => Obx(
