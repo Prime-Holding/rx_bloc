2449d72f1ca761c71d7f921dbf6a7c30be1b9433
RomanovaPrime <snezhana.romanova@primeholding.com>
2021-05-28 17:06:45 +0300

    Add the "copyright" part in front of all .dart files, except those that are autogenerated

--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/count_data_source/count_data_source.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/count_data_source/count_data_source.dart
@@ -1,5 +1,14 @@
+// Copyright (c) 2021, Prime Holding JSC
+// https://www.primeholding.com
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 import '../../base/models/count.dart';
 
+/// A contract class containing all methods,
+/// that  should be implemented by real data source
 abstract class CountDataSource{
   Future<Count> getCurrent();
   Future<Count> increment();
--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/count_data_source/count_local_data_source.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/count_data_source/count_local_data_source.dart
@@ -1,3 +1,10 @@
+// Copyright (c) 2021, Prime Holding JSC
+// https://www.primeholding.com
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 import '../../base/models/count.dart';
 
 import 'count_data_source.dart';
