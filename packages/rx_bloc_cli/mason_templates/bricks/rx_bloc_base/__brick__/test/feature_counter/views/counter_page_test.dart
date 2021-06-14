5b5e386ef0e934e54e2066a3742f43101ed1af00
Georgi Stanev <georgi.stanev@primeholding.com>
2021-06-14 17:11:06 +0300

    Final touches

--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/test/feature_counter/views/counter_page_test.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/test/feature_counter/views/counter_page_test.dart
@@ -5,22 +5,23 @@ import '../../helpers/models/scenario.dart';
 import 'factories/counter_page_factory.dart';
 
 void main() {
-  group('CounterPage golden tests', () {
-    final deviceBuilders = [
-      generateDeviceBuilder(
-        scenario: Scenario(name: 'counter'),
-        widget: counterPageFactory(count: 2),
-      ),
-      generateDeviceBuilder(
-        scenario: Scenario(name: 'error'),
-        widget: counterPageFactory(count: 2, error: 'Test errors'),
-      ),
-      generateDeviceBuilder(
-        scenario: Scenario(name: 'loading'),
-        widget: counterPageFactory(count: 2, isLoading: true),
-      ),
-    ];
-
-    runGoldenTests(deviceBuilders);
-  });
+  group(
+    'CounterPage golden tests',
+    () => runGoldenTests(
+      [
+        generateDeviceBuilder(
+          scenario: Scenario(name: 'counter'),
+          widget: counterPageFactory(count: 2),
+        ),
+        generateDeviceBuilder(
+          scenario: Scenario(name: 'error'),
+          widget: counterPageFactory(count: 2, error: 'Test errors'),
+        ),
+        generateDeviceBuilder(
+          scenario: Scenario(name: 'loading'),
+          widget: counterPageFactory(count: 2, isLoading: true),
+        ),
+      ],
+    ),
+  );
 }
