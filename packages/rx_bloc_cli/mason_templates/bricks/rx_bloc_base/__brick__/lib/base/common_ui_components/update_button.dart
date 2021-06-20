5f9008a9ce51cf4570b43684c4e1e67b05f325a6
Georgi Stanev <georgi.stanev@primeholding.com>
2021-06-20 10:50:35 +0300

    Widget catalog improvements

--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/common_ui_components/update_button.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/common_ui_components/update_button.dart
@@ -4,22 +4,25 @@ import '../../base/theme/design_system.dart';
 import '../../l10n/l10n.dart';
 
 class UpdateButton extends StatelessWidget {
-  const UpdateButton({required isActive, required onPressed, Key? key})
-      : _isActive = isActive,
+  const UpdateButton({
+    required bool isActive,
+    required VoidCallback onPressed,
+    Key? key,
+  })  : _isActive = isActive,
         _onPressed = onPressed,
         super(key: key);
 
-  final void Function() _onPressed;
+  final VoidCallback _onPressed;
 
   final bool _isActive;
 
   @override
   Widget build(BuildContext context) => IconButton(
-      onPressed: _isActive ? _onPressed : null,
-      icon: Icon(context.designSystem.icons.reload),
-      tooltip: context.l10n.reload,
-      color: _isActive
-          ? context.designSystem.colors.activeButtonTextColor
-          : context.designSystem.colors.inactiveButtonTextColor,
-    );
+        onPressed: _isActive ? _onPressed : null,
+        icon: Icon(context.designSystem.icons.reload),
+        tooltip: context.l10n.reload,
+        color: _isActive
+            ? context.designSystem.colors.activeButtonTextColor
+            : context.designSystem.colors.inactiveButtonTextColor,
+      );
 }
