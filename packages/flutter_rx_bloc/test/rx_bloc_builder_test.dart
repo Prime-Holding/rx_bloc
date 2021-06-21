72409122bbdd36e737813e58faf1bddae59c135e
Georgi Stanev <georgi.stanev@primeholding.com>
2021-06-21 14:45:14 +0300

    Refactoring

--- packages/flutter_rx_bloc/test/rx_bloc_builder_test.dart
+++ packages/flutter_rx_bloc/test/rx_bloc_builder_test.dart
@@ -2,25 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rx_bloc/flutter_rx_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'blocs/counter_bloc.dart';
-
-const _countKey = 'count_key';
-const _noCountKey = 'no_count_key';
-
-Widget _buildBlocBuilder({CounterBlocType? bloc}) {
-  return RxBlocBuilder<CounterBlocType, int>(
-    state: (bloc) => bloc.states.count,
-    bloc: bloc,
-    builder: (context, snapshot, bloc) => snapshot.hasData
-        ? Text(
-            snapshot.data.toString(),
-            key: const Key(_countKey),
-          )
-        : Container(
-            key: const Key(_noCountKey),
-          ),
-  );
-}
+import 'counter/blocs/counter_bloc.dart';
+import 'counter/views/counter_widget.dart';
 
 void main() {
   group('BlocBuilder', () {
@@ -29,7 +12,7 @@ void main() {
         MaterialApp(
           home: RxBlocProvider<CounterBlocType>(
             create: (ctx) => CounterBloc(countEvents: [1, 2]),
-            child: _buildBlocBuilder(),
+            child: const CounterWidget(),
           ),
         ),
       );
@@ -37,7 +20,7 @@ void main() {
       await tester.pumpAndSettle();
 
       expect(
-        tester.widget<Text>(find.byKey(const Key(_countKey))).data,
+        tester.widget<Text>(find.byKey(const Key(CounterWidget.countKey))).data,
         '2',
       );
     });
@@ -45,7 +28,7 @@ void main() {
     testWidgets('Build with direct instance', (tester) async {
       await tester.pumpWidget(
         MaterialApp(
-          home: _buildBlocBuilder(
+          home: CounterWidget(
             bloc: CounterBloc(countEvents: [1, 2]),
           ),
         ),
@@ -54,7 +37,7 @@ void main() {
       await tester.pumpAndSettle();
 
       expect(
-        tester.widget<Text>(find.byKey(const Key(_countKey))).data,
+        tester.widget<Text>(find.byKey(const Key(CounterWidget.countKey))).data,
         '2',
       );
     });
@@ -62,7 +45,7 @@ void main() {
     testWidgets('Snapshot has no data', (tester) async {
       await tester.pumpWidget(
         MaterialApp(
-          home: _buildBlocBuilder(
+          home: CounterWidget(
             bloc: CounterBloc(countEvents: []),
           ),
         ),
@@ -71,12 +54,14 @@ void main() {
       await tester.pumpAndSettle();
 
       expect(
-        tester.widgetList(find.byKey(const Key(_countKey))).length,
+        tester.widgetList(find.byKey(const Key(CounterWidget.countKey))).length,
         0,
       );
 
       expect(
-        tester.widgetList(find.byKey(const Key(_noCountKey))).length,
+        tester
+            .widgetList(find.byKey(const Key(CounterWidget.noCountKey)))
+            .length,
         1,
       );
     });
