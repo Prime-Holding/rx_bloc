86ce1c56011ca9a7990c11e3e3ceef7573ffa5fa
Georgi Stanev <georgi.stanev@primeholding.com>
2021-09-20 13:55:24 +0300

    Fix tests

--- packages/rx_bloc_list/lib/src/models/paginated_list.dart
+++ packages/rx_bloc_list/lib/src/models/paginated_list.dart
@@ -146,23 +146,23 @@ class PaginatedList<E> extends ListBase<E> {
   @override
   operator ==(other) =>
       other is PaginatedList<E> &&
-      other.list == list &&
       other.pageSize == pageSize &&
       other.error == error &&
       other.totalCount == totalCount &&
       other.isLoading == isLoading &&
-      other._isInitialized == _isInitialized;
+      other.list == list;
 
   @override
   int get hashCode =>
-      list.hashCode ^
       pageSize.hashCode ^
       error.hashCode ^
       totalCount.hashCode ^
       isLoading.hashCode ^
-      _isInitialized.hashCode;
+      _isInitialized.hashCode ^
+      list.hashCode;
 
   @override
   String toString() =>
-      '{list: $list, _backupList: $_backupList, loading: $isLoading}';
+      '{pageSize: $pageSize, error: $error, totalCount: $totalCount, '
+      'isLoading: $isLoading, list: $list}';
 }
