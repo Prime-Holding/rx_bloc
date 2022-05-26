64408d5fb86e7c72575cfec0fd7e33624cb5f301
Georgi Stanev <georgi.stanev@primeholding.com>
2022-05-26 09:39:02 +0300

    Increase code coverage on the Result class

--- packages/rx_bloc/test/result_test.dart
+++ packages/rx_bloc/test/result_test.dart
@@ -2,19 +2,55 @@ import 'package:rx_bloc/rx_bloc.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Result<T>', () {
-    test('positive equals', () {
+  group('Result<String>', () {
+    test('positive equals String', () {
+      final result = Result<String>.success('1');
+      final resultTwo = Result<String>.success('1');
+
+      expect(result, equals(resultTwo));
+    });
+
+    test('negative equals String', () {
+      final result = Result<String>.success('1');
+      final resultTwo = Result<String>.success('2');
+
+      expect(result, isNot(equals(resultTwo)));
+    });
+  });
+
+  group('Result<List<T>>', () {
+    test('positive equals List', () {
       final result = Result<List<int>>.success([1, 2, 3]);
       final resultTwo = Result<List<int>>.success([1, 2, 3]);
 
       expect(result, equals(resultTwo));
     });
 
-    test('negative equals', () {
+    test('negative equals List', () {
       final result = Result<List<int>>.success([1, 2, 3]);
       final resultTwo = Result<List<int>>.success([1, 2]);
 
       expect(result, isNot(equals(resultTwo)));
+    });
+  });
+
+  group('Result<Map<T>>', () {
+    test('positive equals Map', () {
+      final result = Result<Map<String, int>>.success({'a': 1, 'b': 2});
+      final resultTwo = Result<Map<String, int>>.success({'a': 1, 'b': 2});
+
+      expect(result, equals(equals(resultTwo)));
+    });
+
+    test('negative equals Map', () {
+      final result = Result<Map<String, int>>.success({'a': 1, 'b': 2});
+      final resultTwoA = Result<Map<String, int>>.success({'asd': 1, 'b': 2});
+
+      expect(result, isNot(equals(resultTwoA)));
+
+      final resultTwoB = Result<Map<String, int>>.success({'a': 2, 'b': 2});
+
+      expect(result, isNot(equals(resultTwoB)));
     });
   });
 }
