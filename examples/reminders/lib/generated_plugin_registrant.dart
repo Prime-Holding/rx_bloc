a105ca526d6fe11bc345a7d8cb3f56ae1be82e11
Stanislav Petkov <stanislav.petkov@primeholding.com>
2022-06-08 11:32:33 +0300

    Add firebase projects configuration settings

--- examples/reminders/lib/generated_plugin_registrant.dart
+++ examples/reminders/lib/generated_plugin_registrant.dart
@@ -8,6 +8,7 @@
 
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_database_web/firebase_database_web.dart';
 import 'package:firebase_messaging_web/firebase_messaging_web.dart';
 import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
@@ -18,6 +19,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   FirebaseAnalyticsWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FirebaseDatabaseWeb.registerWith(registrar);
   FirebaseMessagingWeb.registerWith(registrar);
   FlutterSecureStorageWeb.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
