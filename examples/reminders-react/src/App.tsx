7f8176e3aa26295e4015eae58b21e526b6fe6f99
Viktor Angeliev <viktor.angeliev@primeholding.com>
2022-08-11 15:50:17 +0300

    Remove material theme

--- examples/reminders-react/src/App.tsx
+++ examples/reminders-react/src/App.tsx
@@ -2,20 +2,16 @@ import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './features/authentication/state/AuthProvider';
 import './style/app.scss';
 import AppRoutes from './routes/AppRoutes';
-import { ThemeProvider } from '@mui/material';
-import theme from './theme/theme';
 import { SnackbarProvider } from 'notistack';
 
 const App = () => {
 	return (
 		<AuthProvider>
-			<ThemeProvider theme={theme}>
-				<SnackbarProvider anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}>
-					<BrowserRouter>
-						<AppRoutes />
-					</BrowserRouter>
-				</SnackbarProvider>
-			</ThemeProvider>
+			<SnackbarProvider anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}>
+				<BrowserRouter>
+					<AppRoutes />
+				</BrowserRouter>
+			</SnackbarProvider>
 		</AuthProvider>
 	);
 };
