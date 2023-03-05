c9cc5633328d7609734f6cf3a542d358af273a3b
Georgi Stanev <georgi.stanev@primeholding.com>
2023-03-06 01:54:51 +0200

    Code clean up

--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/hooks/post_gen.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/hooks/post_gen.dart
@@ -1,25 +1,6 @@
-import 'dart:io';
-
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  context.logger.info('hello Georgi!');
+  // Add your post-generation code here
+  // (Code executed immediately after the generation step)
 }
-
-// void run(HookContext context) async {
-//   // Add your post-generation code here
-//   // (Code executed immediately after the generation step)
-//
-//   //Builds generated packages
-//   final formatProgress = context.logger.progress(
-//       'Running "dart pub run build_runner build --delete-conflicting-outputs"');
-//   await Process.run('dart', ['pub', 'get']);
-//   await Process.run('dart', [
-//     'run',
-//     'build_runner',
-//     'build',
-//     '--delete-conflicting-outputs',
-//   ]);
-//   await Process.run('dart', ['format', '.']);
-//   formatProgress.complete();
-// }
