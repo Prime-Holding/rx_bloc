4440e67d80fc0ee23f488094906fd8a759178b1f
zdravkoc <127844831+zdravkoc@users.noreply.github.com>
2023-04-19 21:02:27 +0200

    cancel social login error handling improved

--- packages/rx_bloc_cli/mason_templates/bricks/lib_social_logins/__brick__/lib/lib_social_logins/repositories/google_auth_repository.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/lib_social_logins/__brick__/lib/lib_social_logins/repositories/google_auth_repository.dart
@@ -4,7 +4,6 @@ import '../../base/common_mappers/error_mappers/error_mapper.dart';
 import '../../lib_auth/models/auth_token_model.dart';
 import '../data_sources/google_auth_data_source.dart';
 import '../data_sources/google_credential_data_source.dart';
-import '../models/cancelled_error_model.dart';
 import '../models/google_auth_request_model.dart';
 import '../models/google_credentials_model.dart';
 
@@ -28,10 +27,6 @@ class GoogleAuthRepository {
       _errorMapper.execute(() async {
         final credentials =
             await _googleCredentialDataSource.getUsersGoogleCredential();
-
-        if (credentials == null) {
-          throw CancelledErrorModel();
-        }
 
         return GoogleCredentialsModel.fromGoogleCredentials(credentials);
       });
