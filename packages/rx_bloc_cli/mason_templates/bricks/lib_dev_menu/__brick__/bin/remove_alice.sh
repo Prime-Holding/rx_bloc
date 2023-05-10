c782d393132f36ece726822a5a797752f886660a
zdravkoc <127844831+zdravkoc@users.noreply.github.com>
2023-05-10 09:51:50 +0200

    remove alice shell script added

---
+++ packages/rx_bloc_cli/mason_templates/bricks/lib_dev_menu/__brick__/bin/remove_alice.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# NOTE: this script should be executed in fastlane only
+
+if flutter pub deps | grep -q alice ; then flutter pub remove alice; fi
+
+if [ -e lib/lib_dev_menu ]; then rm -r lib/lib_dev_menu; fi
+if [ -e lib/main_dev.dart ]; then rm lib/main_dev.dart; fi
+if [ -e lib/main_staging.dart ]; then rm lib/main_staging.dart; fi
