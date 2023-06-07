9377198e1ef93a4d2737d47d1c4be0fbc68d47ad
JvnSlv <jovan.slavkovic@primeholding.com>
2023-06-07 10:46:56 +0200

    Remove unnecessary state from notifications bloc

--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/common_blocs/push_notifications_bloc.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/common_blocs/push_notifications_bloc.dart
@@ -10,35 +10,29 @@ part 'push_notifications_bloc.rxb.g.dart';
 
 /// A contract class containing all events of the PushNotificationsBloC.
 abstract class PushNotificationsBlocEvents {
-  /// Event for handling opening push notifications from background
-  /// or from in-app
   void tapOnEvent(NotificationModel event);
 }
 
 /// A contract class containing all states of the PushNotificationsBloC.
-abstract class PushNotificationsBlocStates {
-  ///State used for handling routing on push notification recived
-  ConnectableStream<void> get onRouting;
-}
+abstract class PushNotificationsBlocStates {}
 
 @RxBloc()
 class PushNotificationsBloc extends $PushNotificationsBloc {
   PushNotificationsBloc(this._routerBloc) {
-    onRouting.connect().addTo(_compositeSubscription);
+    _onRedirectingNotification.connect().addTo(_compositeSubscription);
   }
 
   final RouterBlocType _routerBloc;
 
-  @override
-  ConnectableStream<void> _mapToOnRoutingState() =>
+  late final ConnectableStream<void> _onRedirectingNotification =
       _$tapOnEventEvent.asyncMap<void>((event) {
-        switch (event.type) {
-          case NotificationModelType.dashboard:
-            return _routerBloc.events.go(const DashboardRoute());
-          case NotificationModelType.profile:
-            return _routerBloc.events.go(const ProfileRoute());
-          default:
-            null;
-        }
-      }).publish();
+    switch (event.type) {
+      case NotificationModelType.dashboard:
+        return _routerBloc.events.go(const DashboardRoute());
+      case NotificationModelType.profile:
+        return _routerBloc.events.go(const ProfileRoute());
+      default:
+        null;
+    }
+  }).publish();
 }
