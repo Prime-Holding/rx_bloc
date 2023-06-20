84834d54ec6e7062c87594e8594aee762e57f8dc
zdravkoc <127844831+zdravkoc@users.noreply.github.com>
2023-06-20 18:37:38 +0200

    MaterialApp removed from otp screen

--- packages/rx_bloc_cli/mason_templates/bricks/feature_otp/__brick__/lib/feature_otp/views/otp_screen.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/feature_otp/__brick__/lib/feature_otp/views/otp_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:widget_toolkit/text_field_dialog.dart';
-import 'package:widget_toolkit/theme_data.dart';
 import 'package:widget_toolkit_otp/widget_toolkit_otp.dart';
 
 import '../../app_extensions.dart';
@@ -14,75 +13,60 @@ class OtpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String phoneNumber = context.l10n.featureOtp.phoneNumber;
-    return MaterialApp(
-      theme: ThemeData.light()
-          .copyWith(colorScheme: ColorScheme.fromSwatch(), extensions: [
-        WidgetToolkitTheme.light,
-        SmsCodeTheme.light,
-        TextFieldDialogTheme.light,
-      ]),
-      darkTheme: ThemeData.dark()
-          .copyWith(colorScheme: ColorScheme.fromSwatch(), extensions: [
-        WidgetToolkitTheme.dark,
-        SmsCodeTheme.dark,
-        TextFieldDialogTheme.dark,
-      ]),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(context.l10n.featureOtp.otpPageTitle),
-        ),
-        body: SafeArea(
-          child: SmsCodeProvider(
-            sentNewCodeActivationTime: 2,
-            smsCodeService: CustomSmsCodeService(context.read()),
-            builder: (state) => Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SmsPhoneNumberField(
-                    builder: (context, number, onChanged) => TextFieldDialog(
-                      label: phoneNumber,
-                      value: number,
-                      validator: OtpTextFieldValidator(),
-                      translateError: (Object error) => null,
-                      onChanged: onChanged,
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(context.l10n.featureOtp.otpPageTitle),
+      ),
+      body: SafeArea(
+        child: SmsCodeProvider(
+          sentNewCodeActivationTime: 2,
+          smsCodeService: CustomSmsCodeService(context.read()),
+          builder: (state) => Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SmsPhoneNumberField(
+                  builder: (context, number, onChanged) => TextFieldDialog(
+                    label: phoneNumber,
+                    value: number,
+                    validator: OtpTextFieldValidator(),
+                    translateError: (Object error) => null,
+                    onChanged: onChanged,
                   ),
-                  Column(
+                ),
+                Column(
+                  children: [
+                    Text(
+                      context.l10n.featureOtp.hint,
+                      style: TextStyle(color: context.designSystem.colors.gray),
+                    ),
+                    const SizedBox(height: 8),
+                    const SmsCodeField(),
+                    const SizedBox(height: 8),
+                    const ValidityWidget(),
+                  ],
+                ),
+                SizedBox(
+                  height: 85,
+                  child: Column(
                     children: [
-                      Text(
-                        context.l10n.featureOtp.hint,
-                        style:
-                            TextStyle(color: context.designSystem.colors.gray),
+                      ResendCodeButton(
+                        activeStateIcon: Icon(
+                          Icons.send,
+                          color: context.designSystem.colors.primaryColor,
+                        ),
+                        pressedStateIcon: Icon(
+                          Icons.check_circle_outline,
+                          color:
+                              context.designSystem.colors.pinSuccessBorderColor,
+                        ),
                       ),
-                      const SizedBox(height: 8),
-                      const SmsCodeField(),
-                      const SizedBox(height: 8),
-                      const ValidityWidget(),
+                      const ResendButtonTimer(),
                     ],
                   ),
-                  SizedBox(
-                    height: 85,
-                    child: Column(
-                      children: [
-                        ResendCodeButton(
-                          activeStateIcon: Icon(
-                            Icons.send,
-                            color: context.designSystem.colors.primaryColor,
-                          ),
-                          pressedStateIcon: Icon(
-                            Icons.check_circle_outline,
-                            color: context
-                                .designSystem.colors.pinSuccessBorderColor,
-                          ),
-                        ),
-                        const ResendButtonTimer(),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
