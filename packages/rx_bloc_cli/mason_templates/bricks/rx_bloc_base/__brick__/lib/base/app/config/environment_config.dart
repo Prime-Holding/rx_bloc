d332918d3a2e72006c567fb1cd30d6acb529275d
zdravkoc <127844831+zdravkoc@users.noreply.github.com>
2023-07-12 16:29:05 +0200

    Enhanced environment enum

--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/app/config/environment_config.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/app/config/environment_config.dart
@@ -2,33 +2,31 @@
 
 import 'dart:io';
 
-enum EnvironmentType { development, staging, production }
+enum EnvironmentConfig {
+  development(
+    environment: 'development',
+    androidEmulatorBaseApiUrl: 'http://10.0.2.2:8080',
+    iosSimulatorBaseApiUrl: 'http://0.0.0.0:8080'),
 
-/// Environment configuration that enables you to define and read
-/// environment specific properties (such as API endpoints, server secrets, ...)
-class EnvironmentConfig {
-  /// region Environments
+  staging(
+    environment: 'staging',
+    androidEmulatorBaseApiUrl: 'http://10.0.2.2:8080',
+    iosSimulatorBaseApiUrl: 'http://0.0.0.0:8080'),
 
-  const EnvironmentConfig.development()
-      : environment = EnvironmentType.development,
-        androidEmulatorBaseApiUrl = 'http://10.0.2.2:8080',
-        iosSimulatorBaseApiUrl = 'http://0.0.0.0:8080';
+  production(
+    environment: 'production',
+    androidEmulatorBaseApiUrl: 'http://10.0.2.2:8080',
+    iosSimulatorBaseApiUrl: 'http://0.0.0.0:8080');
 
-  const EnvironmentConfig.staging()
-      : environment = EnvironmentType.staging,
-        androidEmulatorBaseApiUrl = 'http://10.0.2.2:8080',
-        iosSimulatorBaseApiUrl = 'http://0.0.0.0:8080';
-
-  const EnvironmentConfig.production()
-      : environment = EnvironmentType.production,
-        androidEmulatorBaseApiUrl = 'http://10.0.2.2:8080',
-        iosSimulatorBaseApiUrl = 'http://0.0.0.0:8080';
-
-  ///endregion
-
-  final EnvironmentType environment;
+  final String environment;
   final String androidEmulatorBaseApiUrl;
   final String iosSimulatorBaseApiUrl;
+
+  const EnvironmentConfig({
+    required this.environment,
+    required this.androidEmulatorBaseApiUrl,
+    required this.iosSimulatorBaseApiUrl,
+  });
 
   String get baseUrl =>
       Platform.isIOS ? iosSimulatorBaseApiUrl : androidEmulatorBaseApiUrl;
