52d9b14a042d7d29fead7fe78d8da5434af9ffe0
zdravkoc <127844831+zdravkoc@users.noreply.github.com>
2023-07-13 10:45:11 +0200

    Removed string environment from environment_config.dart

--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/app/config/environment_config.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/app/config/environment_config.dart
@@ -4,26 +4,21 @@ import 'dart:io';
 
 enum EnvironmentConfig {
   development(
-    environment: 'development',
     androidEmulatorBaseApiUrl: 'http://10.0.2.2:8080',
     iosSimulatorBaseApiUrl: 'http://0.0.0.0:8080'),
 
   staging(
-    environment: 'staging',
     androidEmulatorBaseApiUrl: 'http://10.0.2.2:8080',
     iosSimulatorBaseApiUrl: 'http://0.0.0.0:8080'),
 
   production(
-    environment: 'production',
     androidEmulatorBaseApiUrl: 'http://10.0.2.2:8080',
     iosSimulatorBaseApiUrl: 'http://0.0.0.0:8080');
 
-  final String environment;
   final String androidEmulatorBaseApiUrl;
   final String iosSimulatorBaseApiUrl;
 
   const EnvironmentConfig({
-    required this.environment,
     required this.androidEmulatorBaseApiUrl,
     required this.iosSimulatorBaseApiUrl,
   });
