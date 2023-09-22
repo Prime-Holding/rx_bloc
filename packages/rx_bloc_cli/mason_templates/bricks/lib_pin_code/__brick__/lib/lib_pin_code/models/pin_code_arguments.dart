937664a740496d93cb400d1b5794150075bf7881
JvnSlv <jovan.slavkovic@primeholding.com>
2023-09-22 13:06:14 +0200

    Fix biometrics button showing on confirm pin screen

--- packages/rx_bloc_cli/mason_templates/bricks/lib_pin_code/__brick__/lib/lib_pin_code/models/pin_code_arguments.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/lib_pin_code/__brick__/lib/lib_pin_code/models/pin_code_arguments.dart
@@ -5,9 +5,11 @@ class PinCodeArguments {
     required this.title,
     this.isSessionTimeout = false,
     this.onReturn,
+    this.showBiometricsButton = false,
   });
 
   final String title;
   final bool isSessionTimeout;
   final Function? onReturn;
+  final bool showBiometricsButton;
 }
