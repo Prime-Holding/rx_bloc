d08d7afed8a967943da514b3a0cd1204f705f096
Krasimir Petkov <krasimir.petkov@primeholding.com>
2023-10-11 14:51:15 +0300

    Add analytics bloc

---
+++ packages/rx_bloc_cli/mason_templates/bricks/lib_analytics/__brick__/lib/lib_analytics/blocs/analytics_bloc.dart
@@ -0,0 +1,40 @@
+import 'package:rx_bloc/rx_bloc.dart';
+import 'package:rxdart/rxdart.dart';
+
+import '../../base/common_blocs/coordinator_bloc.dart';
+import '../../base/models/errors/error_model.dart';
+import '../services/analytics_service.dart';
+
+part 'analytics_bloc.rxb.g.dart';
+
+/// A contract class containing all events of the FirebaseBloc.
+abstract class AnalyticsBlocEvents {}
+
+/// A contract class containing all states of the FirebaseBloc.
+abstract class AnalyticsBlocStates {}
+
+@RxBloc()
+class AnalyticsBloc extends $AnalyticsBloc {
+  AnalyticsBloc(
+      CoordinatorBlocType coordinator,
+      AnalyticsService service,
+      ) {
+    coordinator.states.errorLogEvent.listen(
+          (event) {
+        service.recordError(
+          event.error,
+          StackTrace.fromString(
+              '#0     ${_getMethodName(event.error)} (${_getFileName(event.error)}:1:1)'),
+          event.errorLogDetails,
+        );
+      },
+    ).addTo(_compositeSubscription);
+  }
+
+  String _getFileName(ErrorModel error) =>
+      '${error.runtimeType.toString()}.dart';
+
+  String _getMethodName(ErrorModel model) {
+    return 'businessError';
+  }
+}
