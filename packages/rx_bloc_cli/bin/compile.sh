9e0b8b1a3a3e7e1a4bfe84889ff056f4f26ceab0
David Djordjevic <david.djordjevic@primeholding.com>
2023-10-25 18:45:32 +0200

    Add github workflow for generating rx_bloc_cli projects with different flags

--- packages/rx_bloc_cli/bin/compile.sh
+++ packages/rx_bloc_cli/bin/compile.sh
@@ -1,26 +1,9 @@
 #!/usr/bin/env sh
+
 . $(dirname "$0")/compile_bundles.sh
 rm -rf example/test_app
 mkdir example/test_app
-dart run rx_bloc_cli create \
-  --project-name=testapp \
-  --organisation=com.primeholding \
-  --enable-analytics \
-  --enable-feature-counter \
-  --enable-feature-deeplinks \
-  --enable-feature-widget-toolkit \
-  --enable-login \
-  --enable-social-logins \
-  --enable-change-language \
-  --enable-dev-menu \
-  --enable-patrol \
-  --realtime-communication=sse \
-  --enable-otp \
-  --cicd=fastlane \
-  --enable-pin-code \
-  --enable-auth-matrix \
-  --no-interactive \
-  example/test_app
+$(dirname "$0")/generate_test_project.sh all_enabled
 
 # Copy the readme file one level up so that it is visible on the pub.dev page
 cp example/test_app/README.md example/
