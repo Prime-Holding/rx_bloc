ef5b7799f93d2dd4b2cd8bc3482f1e73dc0c77a6
David Djordjevic <david.djordjevic@primeholding.com>
2023-11-28 15:12:13 +0100

    Fix analysis warnings and errors

--- packages/rx_bloc_generator/lib/src/builders/state_method.dart
+++ packages/rx_bloc_generator/lib/src/builders/state_method.dart
@@ -1,4 +1,4 @@
-part of rx_bloc_generator;
+part of '../../rx_bloc_generator.dart';
 
 /// A mapper that converts a [MethodElement] into an event [Method]
 class _StateMethod implements _BuilderContract {
--- packages/rx_bloc_generator/lib/src/rx_bloc_generator_for_annotation.dart
+++ packages/rx_bloc_generator/lib/src/rx_bloc_generator_for_annotation.dart
@@ -1,4 +1,4 @@
-part of rx_bloc_generator;
+part of '../rx_bloc_generator.dart';
 
 /// The generator.
 class RxBlocGeneratorForAnnotation extends GeneratorForAnnotation<RxBloc> {
