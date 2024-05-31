666fe136c5314f2e0816eb84bca3a30040b2567e
David Djordjevic <david.djordjevic@primeholding.com>
2024-05-31 15:14:52 +0200

    Fix pana score of rx_bloc_generator

--- packages/rx_bloc_generator/lib/src/utilities/extensions.dart
+++ packages/rx_bloc_generator/lib/src/utilities/extensions.dart
@@ -94,13 +94,11 @@ extension _EventMethodElement on MethodElement {
       _rxBlocEventAnnotation?.computeConstantValue();
 
   /// Provides the [RxBlocEvent] annotation as [ElementAnnotation] if exists
-  ElementAnnotation? get _rxBlocEventAnnotation => _eventAnnotation
-              ?.computeConstantValue()
-              ?.type
-              ?.getDisplayString() ==
-          (RxBlocEvent).toString()
-      ? _eventAnnotation
-      : null;
+  ElementAnnotation? get _rxBlocEventAnnotation =>
+      _eventAnnotation?.computeConstantValue()?.type?.getDisplayString() ==
+              (RxBlocEvent).toString()
+          ? _eventAnnotation
+          : null;
 
   /// Is the event stream type a BehaviorSubject
   bool get isBehavior =>
