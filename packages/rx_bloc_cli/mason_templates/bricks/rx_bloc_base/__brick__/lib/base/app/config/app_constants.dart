ecf4643b79224a4399c56a1811b4b09c2640837f
nikolay-vasilev-prime <165766690+nikolay-vasilev-prime@users.noreply.github.com>
2024-07-16 12:26:04 +0300

    Add documentation for isInTestMode usage

--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/app/config/app_constants.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/app/config/app_constants.dart
@@ -4,8 +4,41 @@
 
 // Here lie all your app constants
 
-/// Vapid Key is required in order to for FCM to run on web properly
-/// https://github.com/FirebaseExtended/flutterfire/blob/4c9b5d28de9eeb5ce76c856fbd0c7b3ec8615e45/docs/messaging/usage.mdx#web-tokens
+/// The [isInTestMode] in these files is used to exclude the dependencies
+/// from smart widgets for golden tests.
+///
+/// Since we don't want to add dependency on [universal_io/io.dart],
+/// we check if [dart.library.io] is available to determine
+/// if we are running test or other environment.
+///
+/// It should use it as follows:
+///
+/// import '../../base/app/config/app_constants.dart';
+///
+//// class AppTodoListBulkEditPopupMenuButtonWithDependencies {
+///     @override
+///     Widget build(BuildContext context) {
+///         final current =  TodoManagementPage();
+///
+///         if(isInTestMode) {
+///             return current;
+///         }
+///
+///         return MultiProvider(
+///             providers: [
+///               ..._services,
+///               ..._blocs,
+///            ],
+///            child: current,
+///         );
+///   }
+/// }
+///
+/// IMPORTANT: you should import [isInTestMode] from [app_constants.dart]
+///
+
 export 'is_in_test_mode_io_not_available.dart' if (dart.library.io) 'is_in_test_mode_io_available.dart' if (dart.library.html) 'is_in_test_mode_io_not_available.dart';
 
+/// Vapid Key is required in order to for FCM to run on web properly
+/// https://github.com/FirebaseExtended/flutterfire/blob/4c9b5d28de9eeb5ce76c856fbd0c7b3ec8615e45/docs/messaging/usage.mdx#web-tokens
 const String webVapidKey = '';
--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/app/config/is_in_test_mode_io_available.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/lib/base/app/config/is_in_test_mode_io_available.dart
@@ -1,3 +1,6 @@
+/// Please use the variable from [app_constants.dart]
+/// This file isn't meant to be used as a main source for its contents.
+
 import 'dart:io';
 
 bool isInTestMode = Platform.environment.containsKey('FLUTTER_TEST');
