8d88c33467d7dfc860f799f5b28f2c7cd82bc387
Georgi Stanev <georgi.stanev@primeholding.com>
2024-07-22 12:16:31 +0300

    Fixes and improvements

--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/bin/server/di/server_dependencies.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/bin/server/di/server_dependencies.dart
@@ -7,11 +7,9 @@ import '../controllers/count_controller.dart';{{/enable_feature_counter}}{{#enab
 import '../controllers/deep_links_controller.dart';{{/enable_feature_deeplinks}}
 import '../controllers/permissions_controller.dart';
 import '../controllers/push_notifications_controller.dart';
-import '../controllers/translations_controller.dart';{{#enable_auth_matrix}}
-import '../repositories/auth_matrix_repository.dart';{{/enable_auth_matrix}}{{#has_authentication}}
+import '../controllers/translations_controller.dart';{{#has_authentication}}
 import '../repositories/auth_token_repository.dart';{{/has_authentication}}
-import '../repositories/translations_repository.dart';{{#enable_auth_matrix}}
-import '../services/auth_matrix_service.dart';{{/enable_auth_matrix}}{{#has_authentication}}
+import '../repositories/translations_repository.dart';{{#has_authentication}}
 import '../services/authentication_service.dart';{{/has_authentication}}
 import '../utils/api_controller.dart';
 import '../utils/dependency_injector.dart';
@@ -45,7 +43,7 @@ class ServerDependencies{
     {{#enable_feature_deeplinks}}
     ..addController(DeepLinksController())
     {{/enable_feature_deeplinks}}{{#enable_auth_matrix}}
-    ..addController(AuthMatrixController(di.get()))
+    ..addController(AuthMatrixController())
     {{/enable_auth_matrix}}
     ;
 
