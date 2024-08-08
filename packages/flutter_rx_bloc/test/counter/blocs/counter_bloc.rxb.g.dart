3dc278188476c4191da53d028a550117a17db9dd
David Djordjevic <david.djordjevic@primeholding.com>
2024-08-08 13:01:12 +0200

    Fix flutter_rx_bloc tests issues

---
+++ packages/flutter_rx_bloc/test/counter/blocs/counter_bloc.rxb.g.dart
@@ -0,0 +1,78 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// Generator: RxBlocGeneratorForAnnotation
+// **************************************************************************
+
+part of 'counter_bloc.dart';
+
+/// Used as a contractor for the bloc, events and states classes
+/// @nodoc
+abstract class CounterBlocType extends RxBlocTypeBase {
+  CounterBlocEvents get events;
+  CounterBlocStates get states;
+}
+
+/// [$CounterBloc] extended by the [CounterBloc]
+/// @nodoc
+abstract class $CounterBloc extends RxBlocBase
+    implements CounterBlocEvents, CounterBlocStates, CounterBlocType {
+  final _compositeSubscription = CompositeSubscription();
+
+  /// Тhe [Subject] where events sink to by calling [setCount]
+  final _$setCountEvent = PublishSubject<int>();
+
+  /// Тhe [Subject] where events sink to by calling [setLoading]
+  final _$setLoadingEvent = PublishSubject<void>();
+
+  /// Тhe [Subject] where events sink to by calling [setError]
+  final _$setErrorEvent = PublishSubject<Exception>();
+
+  /// The state of [count] implemented in [_mapToCountState]
+  late final Stream<Result<int>> _countState = _mapToCountState();
+
+  /// The state of [isLoading] implemented in [_mapToIsLoadingState]
+  late final Stream<bool> _isLoadingState = _mapToIsLoadingState();
+
+  /// The state of [errors] implemented in [_mapToErrorsState]
+  late final Stream<String> _errorsState = _mapToErrorsState();
+
+  @override
+  void setCount(int count) => _$setCountEvent.add(count);
+
+  @override
+  void setLoading() => _$setLoadingEvent.add(null);
+
+  @override
+  void setError(Exception error) => _$setErrorEvent.add(error);
+
+  @override
+  Stream<Result<int>> get count => _countState;
+
+  @override
+  Stream<bool> get isLoading => _isLoadingState;
+
+  @override
+  Stream<String> get errors => _errorsState;
+
+  Stream<Result<int>> _mapToCountState();
+
+  Stream<bool> _mapToIsLoadingState();
+
+  Stream<String> _mapToErrorsState();
+
+  @override
+  CounterBlocEvents get events => this;
+
+  @override
+  CounterBlocStates get states => this;
+
+  @override
+  void dispose() {
+    _$setCountEvent.close();
+    _$setLoadingEvent.close();
+    _$setErrorEvent.close();
+    _compositeSubscription.dispose();
+    super.dispose();
+  }
+}
---
+++ packages/rx_bloc_test/test/helpers/details_bloc/details_bloc.rxb.g.dart
@@ -0,0 +1,63 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// Generator: RxBlocGeneratorForAnnotation
+// **************************************************************************
+
+part of 'details_bloc.dart';
+
+/// Used as a contractor for the bloc, events and states classes
+/// @nodoc
+abstract class DetailsBlocType extends RxBlocTypeBase {
+  DetailsBlocEvents get events;
+  DetailsBlocStates get states;
+}
+
+/// [$DetailsBloc] extended by the [DetailsBloc]
+/// @nodoc
+abstract class $DetailsBloc extends RxBlocBase
+    implements DetailsBlocEvents, DetailsBlocStates, DetailsBlocType {
+  final _compositeSubscription = CompositeSubscription();
+
+  /// Тhe [Subject] where events sink to by calling [fetch]
+  final _$fetchEvent = PublishSubject<void>();
+
+  /// Тhe [Subject] where events sink to by calling [setEmail]
+  final _$setEmailEvent = PublishSubject<String>();
+
+  /// The state of [email] implemented in [_mapToEmailState]
+  late final Stream<String> _emailState = _mapToEmailState();
+
+  /// The state of [details] implemented in [_mapToDetailsState]
+  late final Stream<String> _detailsState = _mapToDetailsState();
+
+  @override
+  void fetch() => _$fetchEvent.add(null);
+
+  @override
+  void setEmail(String email) => _$setEmailEvent.add(email);
+
+  @override
+  Stream<String> get email => _emailState;
+
+  @override
+  Stream<String> get details => _detailsState;
+
+  Stream<String> _mapToEmailState();
+
+  Stream<String> _mapToDetailsState();
+
+  @override
+  DetailsBlocEvents get events => this;
+
+  @override
+  DetailsBlocStates get states => this;
+
+  @override
+  void dispose() {
+    _$fetchEvent.close();
+    _$setEmailEvent.close();
+    _compositeSubscription.dispose();
+    super.dispose();
+  }
+}
