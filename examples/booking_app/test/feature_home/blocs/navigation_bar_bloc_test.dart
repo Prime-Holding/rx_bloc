7523fba35a6718b72250a634b28e964b1256b514
David Djordjevic <david.djordjevic@primeholding.com>
2024-08-09 15:15:59 +0200

    Add tests for booking app

--- examples/booking_app/test/feature_home/blocs/navigation_bar_bloc_test.dart
+++ examples/booking_app/test/feature_home/blocs/navigation_bar_bloc_test.dart
@@ -1,29 +1,41 @@
 import 'package:booking_app/feature_home/blocs/navigation_bar_bloc.dart';
-import 'package:favorites_advanced_base/core.dart';
+import 'package:favorites_advanced_base/models.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:rx_bloc_test/rx_bloc_test.dart';
 
-import '../../stubs.dart';
-import '../mock/navigation_bar_mock.dart';
-
-@GenerateMocks([])
 void main() {
-  group('Navigation Bar Bloc tests', () {
-    rxBlocTest<NavigationBarBlocType, List<NavigationItem>>(
-      'Navigation Bar Bloc initial state',
-      build: () async =>
-          navigationBarMock(navigationType: NavigationItemType.search),
-      state: (bloc) => bloc.states.items,
-      expect: [Stub.navBarItemsList],
-    );
+  group('NavigationBarBloc tests', () {
+    late NavigationBarBloc bloc;
 
-    rxBlocTest<NavigationBarBlocType, NavigationItem>(
-      'Navigation Bar Bloc selected item',
-      build: () async =>
-          navigationBarMock(navigationType: NavigationItemType.search),
-      state: (bloc) => bloc.states.selectedItem,
-      expect: [Stub.navBarSelectedItem],
-    );
+    setUp(() {
+      bloc = NavigationBarBloc(NavigationItemType.search);
+    });
+
+    tearDown(() {
+      bloc.dispose();
+    });
+
+    test('initial state is correct', () {
+      final expectedItems = NavigationItemType.values
+          .map((type) => NavigationItem(
+                type: type,
+                isSelected: type == NavigationItemType.search,
+              ))
+          .toList();
+
+      expect(bloc.items, emitsInOrder([expectedItems]));
+    });
+
+    test('title stream emits correct title', () {
+      const expectedTitleSearch = 'Search for Puppies';
+      const expectedTitleFavorites = 'Favorites Puppies';
+
+      bloc.events.selectPage(NavigationItemType.favorites);
+      expect(
+          bloc.title,
+          emitsInOrder([
+            expectedTitleSearch,
+            expectedTitleFavorites,
+          ]));
+    });
   });
 }
