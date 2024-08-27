820ff745e01c66af9e896d832651c8a44eb60ce7
nikolay-vasilev-prime <165766690+nikolay-vasilev-prime@users.noreply.github.com>
2024-08-27 16:10:23 +0300

    Remove counter from HomePageSteps if counter isn't enabled

--- packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/{{#enable_patrol}}integration_test{{/enable_patrol}}/main/steps_utils/home_page_steps.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/rx_bloc_base/__brick__/{{#enable_patrol}}integration_test{{/enable_patrol}}/main/steps_utils/home_page_steps.dart
@@ -4,9 +4,11 @@ import '../configuration/build_app.dart';
 import '../pages/home_page.dart';
 
 class HomePageSteps {
+  {{#enable_feature_counter}}
   static Future<void> navigateToCounterPage(PatrolIntegrationTester $) async {
-    HomePage homePage = HomePage($);{{#enable_feature_counter}}
-    await homePage.tapBtnCounterPage();{{/enable_feature_counter}}{{^enable_feature_counter}}
+    HomePage homePage = HomePage($);
+    await homePage.tapBtnCounterPage();
     // TODO: Implement steps here{{/enable_feature_counter}}
   }
+  {{/enable_feature_counter}}{{^enable_feature_counter}}
 }
