f73c40150c4721ca01271a3407bfc24bc077ac97
nikolay-vasilev-prime <165766690+nikolay-vasilev-prime@users.noreply.github.com>
2024-08-28 12:04:01 +0300

    Run tests if exist

--- packages/rx_bloc_cli/bin/run_test_in_test_project_if_exist.sh
+++ packages/rx_bloc_cli/bin/run_test_in_test_project_if_exist.sh
@@ -10,9 +10,8 @@ files=$(find "$search_dir" -type f -name '*_test.dart')
 # Check if any files were found
 if [ -n "$files" ]; then
     echo "Found _test.dart files!"
-    sh -c 'cd $search_dir'
     flutter pub get && flutter pub run build_runner build --delete-conflicting-outputs
-    flutter test $base_dir
+    flutter test #$base_dir
 else
     echo "No _test.dart files found."
 fi
