aaffdb30940cad81171dafad8e6bf061f90f8243
JvnSlv <jovan.slavkovic@primeholding.com>
2024-09-04 10:01:19 +0200

    Document PermissionMap properties

--- examples/todoapp/lib/lib_permissions/models/permission_map_model.dart
+++ examples/todoapp/lib/lib_permissions/models/permission_map_model.dart
@@ -3,6 +3,9 @@ part 'permission_map_model.realm.dart';
 
 @RealmModel(ObjectType.embeddedObject)
 class $PermissionMap {
+  ///  The name of the permission that is being checked.
   late String permissionName;
+
+  /// Whether the permission is enabled or not.
   late bool isEnabled;
 }
