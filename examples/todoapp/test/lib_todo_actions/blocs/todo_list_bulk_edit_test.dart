d4ab7468dd283d507777e0c68125e6b24bfda982
JvnSlv <jovan.slavkovic@primeholding.com>
2024-09-12 14:35:18 +0200

    Update Patch coverage

--- examples/todoapp/test/lib_todo_actions/blocs/todo_list_bulk_edit_test.dart
+++ examples/todoapp/test/lib_todo_actions/blocs/todo_list_bulk_edit_test.dart
@@ -3,9 +3,12 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:rx_bloc/rx_bloc.dart';
 import 'package:rx_bloc_test/rx_bloc_test.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:todoapp/base/common_blocs/coordinator_bloc.dart';
+import 'package:todoapp/base/models/errors/error_model.dart';
 import 'package:todoapp/base/models/todo_model.dart';
 import 'package:todoapp/base/repositories/todo_repository.dart';
+import 'package:todoapp/lib_router/blocs/router_bloc.dart';
 import 'package:todoapp/lib_todo_actions/blocs/todo_list_bulk_edit_bloc.dart';
 import 'package:todoapp/lib_todo_actions/models/bulk_action.dart';
 import 'package:todoapp/lib_todo_actions/services/todo_actions_service.dart';
@@ -72,4 +75,29 @@ Future<void> main() async {
         [BulkActionModel.markAllIncomplete, BulkActionModel.clearCompleted],
         []
       ]);
+
+  rxBlocTest<TodoListBulkEditBloc, bool>(
+    'test todo_actions_test_dart state _mapToIsLoadingState',
+    build: () async {
+      return bloc();
+    },
+    act: (bloc) async {
+      bloc.events.markAllCompleted();
+    },
+    state: (bloc) => bloc.isLoading,
+    expect: [
+      false,
+    ],
+  );
+
+  rxBlocTest<TodoListBulkEditBloc, ErrorModel>(
+    'emits correct ErrorModel when errorState changes',
+    build: () async {
+      when(repository.deleteCompleted()).thenThrow(NoConnectionErrorModel());
+      return bloc();
+    },
+    act: (bloc) async => bloc.clearCompleted(),
+    state: (bloc) => bloc.errors.doOnData(print),
+    expect: [],
+  );
 }
