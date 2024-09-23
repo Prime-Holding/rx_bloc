779394f6ba5e2c0fed23f4a763b11e66b7b2b4b8
JvnSlv <jovan.slavkovic@primeholding.com>
2024-09-23 21:22:36 +0200

    Revert changes

--- examples/todoapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/todoapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,7 +17,7 @@ import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
