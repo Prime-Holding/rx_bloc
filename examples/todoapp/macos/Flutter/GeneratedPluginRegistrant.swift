0a18eac98b4466ae15c8397007d670a966a49220
JvnSlv <jovan.slavkovic@primeholding.com>
2024-09-23 21:10:29 +0200

    Fastlane config update, connectivity_plus downgrade

--- examples/todoapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/todoapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,7 +17,7 @@ import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
