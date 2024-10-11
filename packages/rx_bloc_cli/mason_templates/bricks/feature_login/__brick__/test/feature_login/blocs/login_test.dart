1dc9063514973ac04b75c9428a6fa21306d51b71
nikolay-vasilev-prime <nikolay.vasilev@primeholding.com>
2024-10-11 16:21:47 +0300

    Update login tests

--- packages/rx_bloc_cli/mason_templates/bricks/feature_login/__brick__/test/feature_login/blocs/login_test.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/feature_login/__brick__/test/feature_login/blocs/login_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:rx_bloc_test/rx_bloc_test.dart';
 import 'package:{{project_name}}/base/common_blocs/coordinator_bloc.dart';
 import 'package:{{project_name}}/base/models/errors/error_model.dart';
@@ -7,6 +8,8 @@ import 'package:{{project_name}}/feature_login/blocs/login_bloc.dart';
 import 'package:{{project_name}}/feature_login/services/login_validator_service.dart';
 import 'package:{{project_name}}/lib_auth/services/user_account_service.dart';
 
+import '../../base/common_blocs/coordinator_bloc_mock.dart';
+import '../stubs.dart';
 import 'login_test.mocks.dart';
 
 @GenerateMocks([
@@ -19,13 +22,49 @@ void main() {
   late UserAccountService userAccountService;
   late LoginValidatorService validatorService;
 
+  void defineWhen({String? username, String? password, ErrorModel? error}) {
+    when(userAccountService.loadPermissions())
+        .thenAnswer((_) => Future.value());
+
+    when(validatorService.validateEmail('')).thenAnswer((_) => '');
+
+    when(validatorService.validatePassword('')).thenAnswer((_) => '');
+
+    if (username != null) {
+      if (error != null) {
+        when(validatorService.validateEmail(username)).thenThrow((_) => error);
+      } else {
+        when(validatorService.validateEmail(username))
+            .thenAnswer((_) => username);
+      }
+    }
+
+    if (password != null) {
+      if (error != null) {
+        when(validatorService.validatePassword(password))
+            .thenThrow((_) => error);
+      } else {
+        when(validatorService.validatePassword(password))
+            .thenAnswer((_) => password);
+      }
+    }
+
+    if (username != null && password != null) {
+      when(userAccountService.login(username: username, password: password))
+          .thenAnswer((_) => Future.value());
+    }
+
+    when(userAccountService.subscribeForNotifications())
+        .thenAnswer((_) => Future.value());
+  }
+
   LoginBloc loginBloc() => LoginBloc(
         coordinatorBloc,
         userAccountService,
         validatorService,
       );
   setUp(() {
-    coordinatorBloc = MockCoordinatorBlocType();
+    coordinatorBloc = coordinatorBlocMockFactory();
     userAccountService = MockUserAccountService();
     validatorService = MockLoginValidatorService();
   });
@@ -33,60 +72,57 @@ void main() {
   group('test login_bloc_dart state email', () {
     rxBlocTest<LoginBlocType, String>('test login_bloc_dart state email',
         build: () async {
+          defineWhen(username: Stubs.email, password: Stubs.password);
           return loginBloc();
         },
-        act: (bloc) async {},
+        act: (bloc) async {
+          bloc.events.setEmail(Stubs.email);
+        },
         state: (bloc) => bloc.states.email,
-        expect: []);
-  });
+        expect: ['', Stubs.email]);
 
-  group('test login_bloc_dart state password', () {
     rxBlocTest<LoginBlocType, String>('test login_bloc_dart state password',
         build: () async {
+          defineWhen(username: Stubs.email, password: Stubs.password);
           return loginBloc();
         },
-        act: (bloc) async {},
+        act: (bloc) async {
+          bloc.events.setPassword(Stubs.password);
+        },
         state: (bloc) => bloc.states.password,
-        expect: []);
-  });
+        expect: ['', Stubs.password]);
 
-  group('test login_bloc_dart state loggedIn', () {
-    rxBlocTest<LoginBlocType, bool>('test login_bloc_dart state loggedIn',
-        build: () async {
+    rxBlocFakeAsyncTest<LoginBlocType, bool>(
+        'test login_bloc_dart state loggedIn',
+        build: () {
+          defineWhen(username: Stubs.email, password: Stubs.password);
           return loginBloc();
         },
-        act: (bloc) async {},
+        act: (bloc, fakeAsync) async {
+          fakeAsync.elapse(const Duration(seconds: 1));
+          bloc.events.login();
+          fakeAsync.elapse(const Duration(seconds: 1));
+          bloc.events.setPassword(Stubs.password);
+          bloc.events.setEmail(Stubs.email);
+          bloc.events.login();
+        },
         state: (bloc) => bloc.states.loggedIn,
-        expect: []);
-  });
+        expect: [false, true]);
 
-  group('test login_bloc_dart state showErrors', () {
-    rxBlocTest<LoginBlocType, bool>('test login_bloc_dart state showErrors',
+    rxBlocTest<LoginBlocType, bool>('test login_bloc_dart state errors',
         build: () async {
+          defineWhen(
+              username: Stubs.email,
+              password: Stubs.password,
+              error: Stubs.error);
           return loginBloc();
         },
-        act: (bloc) async {},
+        act: (bloc) async {
+          bloc.events.setEmail(Stubs.email);
+          bloc.events.setPassword(Stubs.password);
+          bloc.events.login();
+        },
         state: (bloc) => bloc.states.showErrors,
-        expect: []);
-  });
-
-  group('test login_bloc_dart state isLoading', () {
-    rxBlocTest<LoginBlocType, bool>('test login_bloc_dart state isLoading',
-        build: () async {
-          return loginBloc();
-        },
-        act: (bloc) async {},
-        state: (bloc) => bloc.states.isLoading,
-        expect: []);
-  });
-
-  group('test login_bloc_dart state errors', () {
-    rxBlocTest<LoginBlocType, ErrorModel>('test login_bloc_dart state errors',
-        build: () async {
-          return loginBloc();
-        },
-        act: (bloc) async {},
-        state: (bloc) => bloc.states.errors,
-        expect: []);
+        expect: [false, true]);
   });
 }
--- packages/rx_bloc_cli/mason_templates/bricks/lib_auth/__brick__/test/feature_login/stubs.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/lib_auth/__brick__/test/feature_login/stubs.dart
@@ -1,5 +1,13 @@
+{{#enable_login}}import 'package:{{project_name}}/assets.dart';
+import 'package:{{project_name}}/base/models/errors/error_model.dart';
+{{/enable_login}}
 class Stubs {
   static const email = 'something@test.com';
 
-  static const password = 'password';
+  static const password = 'password';{{#enable_login}}
+
+  static final error = FieldErrorModel(
+    errorKey: I18nErrorKeys.invalidEmail,
+    fieldValue: email,
+  );{{/enable_login}}
 }
