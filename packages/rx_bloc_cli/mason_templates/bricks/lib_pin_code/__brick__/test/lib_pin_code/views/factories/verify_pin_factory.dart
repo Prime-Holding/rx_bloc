43b466686a4669f2c3ba0aedc901eef4daf3d014
Goran Djukic <goran.djukic@primeholding.com>
2024-10-17 11:19:11 +0200

    Add PIN code goldens with showBiometrics

--- packages/rx_bloc_cli/mason_templates/bricks/lib_pin_code/__brick__/test/lib_pin_code/views/factories/verify_pin_factory.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/lib_pin_code/__brick__/test/lib_pin_code/views/factories/verify_pin_factory.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rx_bloc/flutter_rx_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:widget_toolkit_biometrics/widget_toolkit_biometrics.dart';
 import 'package:{{project_name}}/lib_pin_code/bloc/update_and_verify_pin_bloc.dart';
 import 'package:{{project_name}}/lib_pin_code/models/pin_code_arguments.dart';
 import 'package:{{project_name}}/lib_pin_code/services/verify_pin_code_service.dart';
 import 'package:{{project_name}}/lib_pin_code/views/verify_pin_code_page.dart';
 import 'package:{{project_name}}/lib_router/blocs/router_bloc.dart';
-import 'package:widget_toolkit_biometrics/widget_toolkit_biometrics.dart';
 
 import '../../../base/common_blocs/router_bloc_mock.dart';
 import '../../mock/verify_pin_code_service_mock.dart';
@@ -15,7 +15,7 @@ import '../mocks/update_and_verify_pin_bloc_mock.dart';
 
 Widget verifyPinFactory({
   String? title,
-  bool? showBiometricsButton,
+  bool showBiometricsButton = false,
 }) =>
     Scaffold(
       body: MultiProvider(
@@ -27,7 +27,9 @@ Widget verifyPinFactory({
             value: routerBlocMockFactory(),
           ),
           Provider<VerifyPinCodeService>.value(
-            value: verifyPinCodeServiceMockFactory(),
+            value: verifyPinCodeServiceMockFactory(
+              showBiometricsButton: showBiometricsButton,
+            ),
           ),
           Provider<BiometricsLocalDataSource>.value(
             value: pinBiometricsLocalDataSourceMockFactory(),
@@ -37,7 +39,7 @@ Widget verifyPinFactory({
           if (title != null) {
             PinCodeArguments pinCodeArguments;
 
-            if (showBiometricsButton != null) {
+            if (showBiometricsButton) {
               pinCodeArguments = PinCodeArguments(
                 title: title,
                 showBiometricsButton: showBiometricsButton,
