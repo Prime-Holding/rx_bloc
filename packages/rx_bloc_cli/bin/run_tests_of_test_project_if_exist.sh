178e000c8b8815a9f32c0a29c4816158d5d341e8
Goran Djukic <goran.djukic@primeholding.com>
2024-11-04 19:28:05 +0100

    Fix CI

--- packages/rx_bloc_cli/bin/run_tests_of_test_project_if_exist.sh
+++ packages/rx_bloc_cli/bin/run_tests_of_test_project_if_exist.sh
@@ -9,7 +9,7 @@ test_files=$(find "$test_directory" -type f -name '*_test.dart')
 # Check if any test_files were found
 if [ -n "$test_files" ]; then
     echo "Found _test.dart in $test_directory folder!"
-    flutter test
+    flutter test --dart-define=GITHUB_ACTIONS=true
 else
     echo "No _test.dart files were found in $test_directory folder."
 fi
