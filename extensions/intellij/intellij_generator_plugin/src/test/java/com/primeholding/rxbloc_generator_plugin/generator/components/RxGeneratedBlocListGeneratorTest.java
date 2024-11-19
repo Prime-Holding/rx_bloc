49cebfbcd51de51df0ae2f4c2f81ee6fa8270368
Pavel Georgiev <pavel.georgiev@primeholding.com>
2024-11-19 16:35:01 +0200

    #846 Fix failing bloc list generator test

--- extensions/intellij/intellij_generator_plugin/src/test/java/com/primeholding/rxbloc_generator_plugin/generator/components/RxGeneratedBlocListGeneratorTest.java
+++ extensions/intellij/intellij_generator_plugin/src/test/java/com/primeholding/rxbloc_generator_plugin/generator/components/RxGeneratedBlocListGeneratorTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+import static java.nio.file.Files.readAllLines;
 import static org.junit.Assert.assertEquals;
 
 public class RxGeneratedBlocListGeneratorTest {
@@ -14,7 +15,7 @@ public class RxGeneratedBlocListGeneratorTest {
     public void testSampleServiceCreation() throws IOException {
         RxGeneratedBlocListGenerator rxBlocGenerator = new RxGeneratedBlocListGenerator("profile_bloc");
         String generate = rxBlocGenerator.generate().trim();
-        File file = new File("src/test/resources/generator/RxGeneratedBlocListGenerator/sample_list_bloc.rxb.g.dart");
+        File file = new File("src/test/resources/generator/RxGeneratedBlocListGenerator/sample_list_bloc.dart");
         String inputRepoText = String.join("\n", Files.readAllLines(file.toPath())).trim();
         assertEquals(generate, inputRepoText);
     }
