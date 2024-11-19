70a0167e8bbe3ef097c64e464c9c1f38af3a8dba
nikolay-vasilev-prime <nikolay.vasilev@primeholding.com>
2024-11-19 10:22:58 +0100

    Revert the rx_bloc_cli test stubs

--- packages/rx_bloc_cli/test/stub.dart
+++ packages/rx_bloc_cli/test/stub.dart
@@ -29,16 +29,16 @@ final class Stub {
   static const licenseFileName = '{{~ licence.dart }}';
 
   static const overwrittenLoginFiles = [
-    'test/feature_login/view/goldens/linux/dark_theme/login_empty.png',
-    'test/feature_login/view/goldens/linux/dark_theme/login_error.png',
-    'test/feature_login/view/goldens/linux/dark_theme/login_filled.png',
-    'test/feature_login/view/goldens/linux/dark_theme/login_loading.png',
-    'test/feature_login/view/goldens/linux/dark_theme/login_success.png',
-    'test/feature_login/view/goldens/linux/light_theme/login_empty.png',
-    'test/feature_login/view/goldens/linux/light_theme/login_error.png',
-    'test/feature_login/view/goldens/linux/light_theme/login_filled.png',
-    'test/feature_login/view/goldens/linux/light_theme/login_loading.png',
-    'test/feature_login/view/goldens/linux/light_theme/login_success.png'
+    'test/feature_login/view/goldens/ci/dark_theme/login_empty.png',
+    'test/feature_login/view/goldens/ci/dark_theme/login_error.png',
+    'test/feature_login/view/goldens/ci/dark_theme/login_filled.png',
+    'test/feature_login/view/goldens/ci/dark_theme/login_loading.png',
+    'test/feature_login/view/goldens/ci/dark_theme/login_success.png',
+    'test/feature_login/view/goldens/ci/light_theme/login_empty.png',
+    'test/feature_login/view/goldens/ci/light_theme/login_error.png',
+    'test/feature_login/view/goldens/ci/light_theme/login_filled.png',
+    'test/feature_login/view/goldens/ci/light_theme/login_loading.png',
+    'test/feature_login/view/goldens/ci/light_theme/login_success.png'
   ];
 
   static Map<String, Object> get defaultValues {
