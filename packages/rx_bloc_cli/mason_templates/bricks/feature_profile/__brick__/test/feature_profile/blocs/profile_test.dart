8d24ad73793853fc39f9675137854afedd82c2a2
JvnSlv <jovan.slavkovic@primeholding.com>
2024-12-02 10:55:14 +0100

    Add profile page tests

--- packages/rx_bloc_cli/mason_templates/bricks/feature_profile/__brick__/test/feature_profile/blocs/profile_test.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/feature_profile/__brick__/test/feature_profile/blocs/profile_test.dart
@@ -1,5 +1,8 @@
+{{> licence.dart }}
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:rx_bloc/rx_bloc.dart';
 import 'package:rx_bloc_test/rx_bloc_test.dart';
 import 'package:{{project_name}}/base/common_services/push_notifications_service.dart';
@@ -14,11 +17,11 @@ import 'profile_test.mocks.dart';
 void main() {
   late PushNotificationsService _notificationService;
 
-  void _defineWhen(/*value*/) {
-    /*
-            //Sample mock during a test case
-            when(repository.fetchPage()).thenAnswer((_) async => value);
-      */
+  void defineWhen({
+    bool? areNotificationsEnabled,
+  }) {
+    when(_notificationService.areNotificationsEnabled())
+        .thenAnswer((_) => Future.value(areNotificationsEnabled));
   }
 
   ProfileBloc profileBloc() => ProfileBloc(
@@ -32,46 +35,68 @@ void main() {
     rxBlocTest<ProfileBlocType, Result<bool>>(
         'test profile_bloc_dart state areNotificationsEnabled',
         build: () async {
-          _defineWhen();
+          defineWhen(
+            areNotificationsEnabled: true,
+          );
           return profileBloc();
         },
-        act: (bloc) async {},
+        act: (bloc) async {
+          bloc.events.loadNotificationsSettings();
+        },
         state: (bloc) => bloc.states.areNotificationsEnabled,
-        expect: []);
+        expect: [
+          Result.loading(),
+          Result.success(true),
+        ]);
   });
 
   group('test profile_bloc_dart state syncNotificationsStatus', () {
     rxBlocTest<ProfileBlocType, Result<bool>>(
         'test profile_bloc_dart state syncNotificationsStatus',
         build: () async {
-          _defineWhen();
+          defineWhen();
           return profileBloc();
         },
-        act: (bloc) async {},
+        act: (bloc) async {
+          bloc.events.setNotifications(true);
+        },
         state: (bloc) => bloc.states.syncNotificationsStatus,
-        expect: []);
+        expect: [
+          Result.loading(),
+          Result.success(true),
+        ]);
   });
 
   group('test profile_bloc_dart state isLoading', () {
     rxBlocTest<ProfileBlocType, bool>('test profile_bloc_dart state isLoading',
         build: () async {
-          _defineWhen();
+          defineWhen();
           return profileBloc();
         },
         act: (bloc) async {},
         state: (bloc) => bloc.states.isLoading,
-        expect: []);
+        expect: [
+          false,
+          true,
+          false,
+        ]);
   });
 
   group('test profile_bloc_dart state errors', () {
     rxBlocTest<ProfileBlocType, ErrorModel>(
         'test profile_bloc_dart state errors',
         build: () async {
-          _defineWhen();
+          defineWhen();
+          when(_notificationService.syncNotificationSettings())
+              .thenAnswer((_) => Future.error(UnknownErrorModel()));
           return profileBloc();
         },
-        act: (bloc) async {},
+        act: (bloc) async {
+          bloc.events.loadNotificationsSettings();
+        },
         state: (bloc) => bloc.states.errors,
-        expect: []);
+        expect: [
+          isA<UnknownErrorModel>(),
+        ]);
   });
 }
