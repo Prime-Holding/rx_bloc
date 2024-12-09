70c3ae10ce2d272be4d3fb0a41a483c81f987d96
David Djordjevic <david.djordjevic@primeholding.com>
2024-12-09 14:23:00 +0100

    Add onboarding brick and cli flag

--- packages/rx_bloc_cli/bin/compile_bundles.sh
+++ packages/rx_bloc_cli/bin/compile_bundles.sh
@@ -123,4 +123,7 @@ dart run mason_cli:mason bundle \
   -o lib/src/templates/ \
   mason_templates/bricks/feature_showcase
 
-
+dart run mason_cli:mason bundle \
+  -t dart \
+  -o lib/src/templates/ \
+  mason_templates/bricks/feature_onboarding
