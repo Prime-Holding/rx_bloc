23890a55c208f31b607d4a39df012eb24e0fd190
Goran Djukic <goran.djukic@primeholding.com>
2024-12-26 11:45:02 +0100

    Revert Register page navigation change

--- packages/rx_bloc_cli/mason_templates/bricks/feature_onboarding/__brick__/lib/feature_onboarding/blocs/onboarding_bloc.dart
+++ packages/rx_bloc_cli/mason_templates/bricks/feature_onboarding/__brick__/lib/feature_onboarding/blocs/onboarding_bloc.dart
@@ -148,10 +148,10 @@ class OnboardingBloc extends $OnboardingBloc {
         return _routerBloc.events.go(const DashboardRoute());
       }
 
-      return _routerBloc.events.push(const OnboardingPhoneRoute());
+      return _routerBloc.events.pushReplace(const OnboardingPhoneRoute());
     }
     return _routerBloc.events
-        .push(OnboardingEmailConfirmationRoute(user.email));
+        .pushReplace(OnboardingEmailConfirmationRoute(user.email));
   }
 
   @override
