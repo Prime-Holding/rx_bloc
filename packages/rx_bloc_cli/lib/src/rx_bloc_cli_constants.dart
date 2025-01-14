a25d0c339187213c441a9ca35a287526a1a766b1
nikolay-vasilev-prime <nikolay.vasilev@primeholding.com>
2025-01-14 18:23:29 +0100

    Upgrade gradle and add desugaring

--- packages/rx_bloc_cli/lib/src/rx_bloc_cli_constants.dart
+++ packages/rx_bloc_cli/lib/src/rx_bloc_cli_constants.dart
@@ -10,5 +10,11 @@ const kAndroidTargetSDKVersion = 34;
 /// Generated project's Android Min SDK version
 const kAndroidMinSDKVersion = 22;
 
+/// Generated project's Android Min SDK version
+const kGradlePluginVersion = "8.3.2";
+
+/// Generated project's Android Min SDK version
+const kGradleWrapperUrl = "https\\://services.gradle.org/distributions/gradle-8.4-all.zip";
+
 /// Different build modes of the flavor
 const kIOSBuildModes = ['Debug', 'Profile', 'Release'];
