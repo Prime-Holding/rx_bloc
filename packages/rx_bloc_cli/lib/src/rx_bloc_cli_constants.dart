955417169f36f0ca1845fe3c2696ce23125676a3
JvnSlv <jovan.slavkovic@primeholding.com>
2026-01-22 14:27:04 +0100

    Remove version 6.0.1

--- packages/rx_bloc_cli/lib/src/rx_bloc_cli_constants.dart
+++ packages/rx_bloc_cli/lib/src/rx_bloc_cli_constants.dart
@@ -1,5 +1,5 @@
 /// Indicates the version of the package
-const rxBlocCliPackageVersion = '6.0.1';
+const rxBlocCliPackageVersion = '6.0.0';
 
 /// Generated project's Android Compile SDK version
 const kAndroidCompileSDKVersion = 36;
